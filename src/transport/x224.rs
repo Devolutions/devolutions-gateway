061d6d0e7a51b2efdcc8bfde6c0042cdfd524f0d
Vlad Alex <vlad.al.dp@gmail.com>
2019-04-02 12:25:20 +0300

    Moved x224 encode/decode to rdp_proto crate

--- src/transport/x224.rs
+++ src/transport/x224.rs
@@ -18,20 +18,7 @@ impl Decoder for X224Transport {
     type Error = io::Error;
 
     fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        let mut stream = buf.as_ref();
-        let len = io_try!(rdp_proto::read_tpkt_len(&mut stream)) as usize;
-
-        if buf.len() < len {
-            return Ok(None);
-        }
-
-        let (_, code) = io_try!(rdp_proto::parse_tdpu_header(&mut stream));
-
-        let mut tpdu = buf.split_to(len as usize);
-        match code {
-            rdp_proto::X224TPDUType::Data => tpdu.advance(rdp_proto::TPDU_DATA_LENGTH),
-            _ => tpdu.advance(rdp_proto::TPDU_REQUEST_LENGTH),
-        }
+        let (code, tpdu) = io_try!(rdp_proto::decode_x224(buf));
 
         Ok(Some((code, tpdu)))
     }
@@ -43,21 +30,12 @@ impl Encoder for X224Transport {
 
     fn encode(&mut self, item: Self::Item, buf: &mut BytesMut) -> Result<(), Self::Error> {
         let (code, data) = item;
-        let length = rdp_proto::TPDU_REQUEST_LENGTH as u16 + data.len() as u16;
 
-        buf.reserve(length as usize);
+        let length = rdp_proto::TPDU_REQUEST_LENGTH + data.len();
+        buf.reserve(length);
         buf.resize(rdp_proto::TPDU_REQUEST_LENGTH, 0);
 
-        let mut buf_slice = buf.as_mut();
-        rdp_proto::write_tpkt_header(&mut buf_slice, length)?;
-        rdp_proto::write_tpdu_header(
-            &mut buf_slice,
-            length as u8 - rdp_proto::TPKT_HEADER_LENGTH as u8,
-            code,
-            0,
-        )?;
-
-        buf.extend_from_slice(&data);
+        rdp_proto::encode_x224(code, data, buf)?;
 
         Ok(())
     }
