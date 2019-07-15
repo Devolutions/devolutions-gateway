c5936a79d9715fbc15a4e74146c68bb1810e502f
Luc Fauvel <lfauvel@devolutions.net>
2019-07-15 12:20:17 -0400

    Fixed devolutions-jet so that it runs on macOS (#19)
    * Fixed issues so that it runs on macos
    
    * Fixed issues so that it runs on macos
    
    * Removed windows code, restricted to openssl

--- tests/rdp.rs
+++ tests/rdp.rs
@@ -543,51 +543,17 @@ where
         .map_err(|e| RdpError::new(format!("Failed to connect to the ssl connection: {}", e)))
 }
 
-#[cfg(target_os = "linux")]
 pub fn get_tls_pubkey(der: &[u8], pass: &str) -> io::Result<Vec<u8>> {
     let cert = openssl::pkcs12::Pkcs12::from_der(der)?.parse(pass)?.cert;
     get_tls_pubkey_from_cert(cert)
 }
 
-#[cfg(target_os = "windows")]
-pub fn get_tls_pubkey(der: &[u8], pass: &str) -> io::Result<Vec<u8>> {
-    let cert_store = schannel::cert_store::PfxImportOptions::new()
-        .password(pass)
-        .import(der)?;
-    for cert in cert_store.certs() {
-        match get_tls_pubkey_from_cert(cert) {
-            Ok(pubkey) => return Ok(pubkey),
-            Err(e) => log::warn!(
-                "An error occurred while trying to get the public key from the certificates store: {}",
-                e
-            ),
-        };
-    }
-
-    Err(io::Error::new(
-        io::ErrorKind::InvalidData,
-        "Failed to find a certificate with a public key",
-    ))
-}
-
-#[cfg(target_os = "linux")]
 pub fn get_tls_peer_pubkey<S>(stream: &native_tls::TlsStream<S>) -> io::Result<Vec<u8>>
 where
     S: io::Read + io::Write,
 {
     let der = get_der_cert_from_stream(&stream)?;
     let cert = openssl::x509::X509::from_der(&der)?;
-
-    get_tls_pubkey_from_cert(cert)
-}
-
-#[cfg(target_os = "windows")]
-pub fn get_tls_peer_pubkey<S>(stream: &native_tls::TlsStream<S>) -> io::Result<Vec<u8>>
-where
-    S: io::Read + io::Write,
-{
-    let der = get_der_cert_from_stream(&stream)?;
-    let cert = schannel::cert_context::CertContext::new(der.as_ref())?;
 
     get_tls_pubkey_from_cert(cert)
 }
@@ -614,12 +580,6 @@ where
         })
 }
 
-#[cfg(target_os = "linux")]
 fn get_tls_pubkey_from_cert(cert: openssl::x509::X509) -> io::Result<Vec<u8>> {
     Ok(cert.public_key()?.public_key_to_der()?.split_off(TLS_PUBLIC_KEY_HEADER))
-}
-
-#[cfg(target_os = "windows")]
-fn get_tls_pubkey_from_cert(cert: schannel::cert_context::CertContext) -> io::Result<Vec<u8>> {
-    Ok(cert.subject_public_key_info_der()?.split_off(TLS_PUBLIC_KEY_HEADER))
 }
