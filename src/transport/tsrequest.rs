8a9302bef87d820ee4fca262b6fe4a0428831141
Vlad Alex <vlad.al.dp@gmail.com>
2019-08-01 16:45:29 +0300

    Refactored transport

--- src/transport/tsrequest.rs
+++ src/transport/tsrequest.rs
@@ -1,16 +1,10 @@
 use std::io;
 
 use bytes::BytesMut;
-use log::debug;
 use tokio::codec::{Decoder, Encoder};
 
+#[derive(Default)]
 pub struct TsRequestTransport {}
-
-impl TsRequestTransport {
-    pub fn new() -> TsRequestTransport {
-        TsRequestTransport {}
-    }
-}
 
 impl Decoder for TsRequestTransport {
     type Item = sspi::TsRequest;
@@ -18,7 +12,6 @@ impl Decoder for TsRequestTransport {
 
     fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
         let ts_request = io_try!(sspi::TsRequest::from_buffer(buf.as_ref()));
-        debug!("Got TSRequest: {:x?}", ts_request);
         buf.split_to(ts_request.buffer_len() as usize);
 
         Ok(Some(ts_request))
@@ -30,7 +23,6 @@ impl Encoder for TsRequestTransport {
     type Error = io::Error;
 
     fn encode(&mut self, item: Self::Item, buf: &mut BytesMut) -> Result<(), Self::Error> {
-        debug!("Send TSRequest: {:x?}", item);
         let len = item.buffer_len();
         buf.resize(len as usize, 0x00);
 
