4ddecab35d9ba8e5d71a6344ac52fba190bc9d9a
Vladyslav Hordiienko <hord.vlad@gmail.com>
2019-10-18 13:24:17 +0300

    Update chan size and overflow strategy for the logger with DEBUG log level

--- src/logger.rs
+++ src/logger.rs
@@ -2,11 +2,13 @@ use std::{env, fs::OpenOptions, io, result::Result, str::FromStr};
 
 use chrono::Utc;
 use slog::{o, warn, Drain, Duplicate, Level, LevelFilter, Logger};
-use slog_async::Async;
+use slog_async::{Async, OverflowStrategy};
 use slog_term::{Decorator, FullFormat, PlainDecorator, TermDecorator};
 
 const LOGGER_TIMESTAMP_FORMAT: &str = "%Y-%m-%d %H:%M:%S:%6f";
 const DEFAULT_LOG_LEVEL: Level = Level::Info;
+const DEFAULT_CHAN_SIZE: usize = 128;
+const DEBUG_CHAN_SIZE: usize = 256;
 
 fn format_decorator(decorator: impl Decorator) -> FullFormat<impl Decorator> {
     FullFormat::new(decorator)
@@ -28,6 +30,12 @@ pub fn init(file_path: Option<&String>) -> io::Result<Logger> {
     let log_level_result = rust_log().unwrap_or(Ok(DEFAULT_LOG_LEVEL));
     let log_level = log_level_result.unwrap_or(DEFAULT_LOG_LEVEL);
 
+    let (chan_size, overflow_strategy) = if log_level > Level::Info {
+        (DEBUG_CHAN_SIZE, OverflowStrategy::Block)
+    } else {
+        (DEFAULT_CHAN_SIZE, OverflowStrategy::DropAndReport)
+    };
+
     let term_decorator = TermDecorator::new().build();
     let term_fmt = format_decorator(term_decorator).fuse();
     let term_drain = LevelFilter::new(term_fmt, log_level).fuse();
@@ -38,14 +46,25 @@ pub fn init(file_path: Option<&String>) -> io::Result<Logger> {
         let file_fmt = format_decorator(file_decorator).fuse();
         let file_drain = LevelFilter::new(file_fmt, log_level).fuse();
 
-        Async::new(Duplicate(file_drain, term_drain).fuse()).build().fuse()
+        Async::new(Duplicate(file_drain, term_drain).fuse())
+            .chan_size(chan_size)
+            .overflow_strategy(overflow_strategy)
+            .build()
+            .fuse()
     } else {
-        Async::new(term_drain).build().fuse()
+        Async::new(term_drain)
+            .chan_size(chan_size)
+            .overflow_strategy(overflow_strategy)
+            .build()
+            .fuse()
     };
     let logger = Logger::root(drain, o!());
 
     if log_level_result.is_err() {
-        warn!(logger, "RUST_LOG value was invalid, setting to the default value: INFO");
+        warn!(
+            logger,
+            "RUST_LOG value was invalid, setting to the default value: {:?}", DEFAULT_LOG_LEVEL
+        );
     }
 
     Ok(logger)
