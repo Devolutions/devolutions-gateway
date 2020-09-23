901147f726a02e2dbb5943033bebb8a781e77673
Benoît CORTIER <benoit.cortier@fried-world.eu>
2020-09-23 12:31:23 -0400

    token/tokengen: update to support RDP-TCP

--- tools/tokengen/src/main.rs
+++ tools/tokengen/src/main.rs
@@ -15,14 +15,6 @@ use std::{error::Error, path::PathBuf, time::SystemTime};
 #[derive(Clap)]
 struct App {
     #[clap(long)]
-    prx_usr: String,
-    #[clap(long)]
-    prx_pwd: String,
-    #[clap(long)]
-    dst_usr: String,
-    #[clap(long)]
-    dst_pwd: String,
-    #[clap(long)]
     dst_hst: String,
     #[clap(long, default_value = "15m")]
     validity_duration: String,
@@ -30,19 +22,37 @@ struct App {
     provider_private_key: PathBuf,
     #[clap(long)]
     jet_public_key: PathBuf,
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    RdpTcp,
+    RdpTls(RdpTlsIdentity),
+}
+
+#[derive(Clap, Serialize)]
+struct RdpTlsIdentity {
+    #[clap(long)]
+    prx_usr: String,
+    #[clap(long)]
+    prx_pwd: String,
+    #[clap(long)]
+    dst_usr: String,
+    #[clap(long)]
+    dst_pwd: String,
 }
 
 #[derive(Serialize)]
 struct RoutingClaims {
     exp: i64,
     nbf: i64,
-    prx_usr: String,
-    prx_pwd: String,
-    dst_usr: String,
-    dst_pwd: String,
-    dst_hst: String,
     jet_cm: String,
     jet_ap: String,
+    dst_hst: String,
+    #[serde(flatten)]
+    identity: Option<RdpTlsIdentity>,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -64,13 +74,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     let claims = RoutingClaims {
         exp: exp as i64,
         nbf: now.as_secs() as i64,
-        prx_usr: app.prx_usr,
-        prx_pwd: app.prx_pwd,
-        dst_usr: app.dst_usr,
-        dst_pwd: app.dst_pwd,
         dst_hst: app.dst_hst,
         jet_cm: "fwd".to_owned(),
         jet_ap: "rdp".to_owned(),
+        identity: if let SubCommand::RdpTls(identity) = app.subcmd {
+            Some(identity)
+        } else {
+            None
+        },
     };
 
     let signed = JwtSig::new(JwsAlg::RS256, claims).encode(&private_key)?;
