ca23a3d8335c2d1061cf94e01859a732b044b090
Benoît CORTIER <benoit.cortier@fried-world.eu>
2020-09-23 12:30:46 -0400

    tools/rdp_token.sh: update to support RDP-TCP

--- tools/rdp_token.sh
+++ tools/rdp_token.sh
@@ -1,42 +1,59 @@
 #!/bin/bash
 
-if [ "$#" -ne 8 ]; then
+if command -v step-cli &> /dev/null; then
+    step_cmd=step-cli
+else
+    if command -v step &> /dev/null; then
+        step_cmd=step
+    else
+        echo "⚠ 'step-cli' not found. Please install it: https://github.com/smallstep/cli#installation"
+        exit
+    fi
+fi
+
+if [ "$#" -ne 8 ] && [ "$#" -ne 4 ]; then
     echo "Illegal number of parameters."
-    echo "Usage: $0 <VALIDITY_DURATION_MINS> \
-<PROVIDER_PRIVATE_KEY> <JET_PUBLIC_KEY> \
-<PRX_USR> <PRX_PWD> \
-<DST_USR> <DST_PWD> <DST_HST>"
+    echo "Usage: $0 <VALIDITY_DURATION_MINS> <PROVIDER_PRIVATE_KEY> <JET_PUBLIC_KEY> <DST_HST> [ <PRX_USR> <PRX_PWD> <DST_USR> <DST_PWD>" ]
     exit
 fi
 
 valid_for_mins="$1"
 provider_key_path="$2"
 jet_public_key_path="$3"
-prx_usr="$4"
-prx_pwd="$5"
-dst_usr="$6"
-dst_pwd="$7"
-dst_hst="$8"
+dst_hst="$4"
 
-claims="{
-    \"jet_ap\": \"rdp\",
-    \"jet_cm\": \"fwd\",
-    \"jet_cm\": \"fwd\",
-    \"prx_usr\": \"${prx_usr}\",
-    \"prx_pwd\": \"${prx_pwd}\",
-    \"dst_usr\": \"${dst_usr}\",
-    \"dst_pwd\": \"${dst_pwd}\",
-    \"dst_hst\": \"${dst_hst}\"
-}"
+if [ "$#" -eq 8 ]; then
+    prx_usr="$5"
+    prx_pwd="$6"
+    dst_usr="$7"
+    dst_pwd="$8"
 
-printf "$claims" | step-cli crypto jwt sign - \
+    claims="{
+        \"jet_ap\": \"rdp\",
+        \"jet_cm\": \"fwd\",
+        \"dst_hst\": \"${dst_hst}\",
+        \"prx_usr\": \"${prx_usr}\",
+        \"prx_pwd\": \"${prx_pwd}\",
+        \"dst_usr\": \"${dst_usr}\",
+        \"dst_pwd\": \"${dst_pwd}\"
+    }"
+else
+    claims="{
+        \"jet_ap\": \"rdp\",
+        \"jet_cm\": \"fwd\",
+        \"dst_hst\": \"${dst_hst}\"
+    }"
+fi
+
+printf "$claims" | ${step_cmd} crypto jwt sign - \
     -nbf $(date "+%s") \
     -exp $(date -d "$valid_for_mins minutes" "+%s") \
     -subtle \
     -key "$provider_key_path" \
     | tr -d '\n' \
-    | step-cli crypto jwe encrypt \
+    | ${step_cmd} crypto jwe encrypt \
     -alg RSA-OAEP-256 \
     -enc A256GCM \
     -key "$jet_public_key_path" \
-    | step-cli crypto jose format
+    | ${step_cmd} crypto jose format
+
