9bdfa06b6f059fbe319303e3ff37525ba1797090
Marc-André Moreau <mamoreau@devolutions.net>
2020-10-07 11:43:41 -0400

    devolutions-jet: update tools

--- tools/DevolutionsGateway.ps1
+++ tools/DevolutionsGateway.ps1
@@ -1,12 +1,14 @@
 
+$script:CompanyDir = "Devolutions"
+$script:ProgramDir = "Gateway"
+$script:GatewayDataPath = [System.Environment]::ExpandEnvironmentVariables("%ProgramData%\${CompanyDir}\${ProgramDir}")
+
 function Register-GatewayService
 {
     $ServiceName = "devolutions-gateway"
     $DisplayName = "Devolutions Gateway"
     $Description = "Devolutions Gateway service"
 
-    $CompanyDir = "Devolutions"
-    $ProgramDir = "Gateway"
     $Executable = "DevolutionsGateway.exe"
 
     $params = @{
@@ -20,19 +22,62 @@ function Register-GatewayService
     New-Service @params
 }
 
+function New-JetKeyPair
+{
+    [CmdletBinding()]
+    param(
+        [string] $PublicKeyFile,
+        [string] $PrivateKeyFile,
+        [ValidateSet(1024,2048,4096)]
+        [int] $KeySize = 2048,
+        [switch] $Force
+    )
+
+    if (-Not $PublicKeyFile) {
+        $PublicKeyFile = Join-Path $GatewayDataPath "public.pem"
+    }
+    
+    if (-Not $PrivateKeyFile) {
+        $PrivateKeyFile = Join-Path $GatewayDataPath "private.key"
+    }
+
+    if (-Not (Test-Path $Path $GatewayDataPath)) {
+        New-Item -Path $GatewayDataPath -ItemType 'Directory' -Force | Out-Null
+    }
+
+    if ((Test-Path -Path $PublicKeyFile) -Or (Test-Path -Path $PrivateKeyFile)) {
+        if (-Not $Force) {
+            throw "$PublicKeyFile or $PrivateKeyFile already exists, use -Force to overwrite"
+        }
+
+        Remove-Item $PublicKeyFile -Force | Out-Null
+        Remove-Item $PrivateKeyFile -Force | Out-Null
+    }
+
+    & 'step' 'crypto' 'keypair' $PublicKeyFile $PrivateKeyFile '--kty' 'RSA' '--size' $KeySize '--insecure' '--no-password'
+}
+
 function New-JetToken
 {
     [CmdletBinding()]
     param(
+        [string] $PublicKeyFile,
+        [string] $PrivateKeyFile,
         [Parameter(Mandatory=$true)]
         [string] $DestinationHost
     )
 
-    $CompanyDir = "Devolutions"
-    $ProgramDir = "Gateway"
+    if (-Not $PublicKeyFile) {
+        $PublicKeyFile = Join-Path $GatewayDataPath "public.pem"
+    }
+    
+    if (-Not $PrivateKeyFile) {
+        $PrivateKeyFile = Join-Path $GatewayDataPath "private.key"
+    }
 
-    $PublicKeyFile = [System.Environment]::ExpandEnvironmentVariables("%ProgramData%\${CompanyDir}\${ProgramDir}\public.pem")
-    $PrivateKeyFile = [System.Environment]::ExpandEnvironmentVariables("%ProgramData%\${CompanyDir}\${ProgramDir}\private.key")
+    if (-Not (Test-Path -Path $PrivateKeyFile)) {
+        throw "$PrivateKeyFile cannot be found."
+    }
 
     $raw_token = [PSCustomObject]@{
         jet_ap = 'rdp'
@@ -56,4 +101,5 @@ function New-JetToken
     #$jwe = "$jwe" | & 'step' 'crypto' 'jose' 'format'
 }
 
-New-JetToken -DestinationHost '192.168.25.158'
+#New-JetKeyPair
+#New-JetToken -DestinationHost '192.168.25.158'
