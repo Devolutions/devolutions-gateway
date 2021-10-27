679c913608e43a5f74b9b8699e61d584b40457d6
Benoît CORTIER <benoit.cortier@fried-world.eu>
2021-10-27 11:09:31 -0400

    Simplify association token claims
    Mostly remove alternative `jet_ap` claim values such as "rdp-tcp".

--- tools/tokengen/src/main.rs
+++ tools/tokengen/src/main.rs
@@ -26,20 +26,20 @@ struct App {
 #[allow(clippy::enum_variant_names)]
 #[derive(Clap)]
 enum SubCommand {
-    RdpTcp(RdpTcpParams),
-    RdpTls(RdpTlsIdentity),
-    RdpTcpRendezvous(RdpTcpRendezvousJetAID),
+    RdpTcp(TcpParams),
+    RdpTls(TlsParams),
+    RdpTcpRendezvous,
     Scope(ScopeParams),
 }
 
 #[derive(Clap)]
-struct RdpTcpParams {
+struct TcpParams {
     #[clap(long)]
     dst_hst: String,
 }
 
 #[derive(Clone, Clap, Serialize)]
-struct RdpTlsIdentity {
+struct TlsParams {
     #[serde(skip_serializing)]
     #[clap(long)]
     jet_public_key: PathBuf,
@@ -57,11 +57,6 @@ struct RdpTlsIdentity {
     dst_pwd: String,
 }
 
-#[derive(Clone, Clap, Serialize)]
-struct RdpTcpRendezvousJetAID {
-    jet_aid: Uuid,
-}
-
 #[derive(Clap)]
 struct ScopeParams {
     #[clap(long)]
@@ -70,23 +65,23 @@ struct ScopeParams {
 
 #[derive(Clone, Serialize)]
 #[serde(tag = "type")]
-enum GatewayAccessClaims {
+enum GatewayAccessClaims<'a> {
     #[serde(rename = "association")]
-    RoutingClaims(RoutingClaims),
+    RoutingClaims(RoutingClaims<'a>),
     #[serde(rename = "scope")]
     ScopeClaims(ScopeClaims),
 }
 
 #[derive(Clone, Serialize)]
-struct RoutingClaims {
+struct RoutingClaims<'a> {
     exp: i64,
     nbf: i64,
-    jet_cm: String,
-    jet_ap: String,
-    jet_aid: Option<Uuid>,
-    dst_hst: Option<String>,
+    jet_cm: &'a str,
+    jet_ap: &'a str,
+    jet_aid: Uuid,
+    dst_hst: Option<&'a str>,
     #[serde(flatten)]
-    identity: Option<RdpTlsIdentity>,
+    identity: Option<TlsParams>,
 }
 
 #[derive(Clone, Serialize)]
@@ -109,19 +104,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     let exp = (now + validity_duration).as_secs();
 
     let jet_cm = match app.subcmd {
-        SubCommand::RdpTcpRendezvous(_) => "rdv".to_owned(),
-        _ => "fwd".to_owned(),
+        SubCommand::RdpTcpRendezvous => "rdv",
+        _ => "fwd",
     };
 
-    let jet_ap = match app.subcmd {
-        SubCommand::RdpTcpRendezvous(_) => "rdp_tcp".to_owned(),
-        _ => "rdp".to_owned(),
-    };
+    let jet_ap = "rdp";
 
-    let jet_aid = match &app.subcmd {
-        SubCommand::RdpTcpRendezvous(rdv) => Some(rdv.jet_aid),
-        _ => None,
-    };
+    let jet_aid = Uuid::new_v4();
 
     let identity = match &app.subcmd {
         SubCommand::RdpTls(identity) => Some(identity.clone()),
@@ -129,8 +118,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     };
 
     let dst_hst = match &app.subcmd {
-        SubCommand::RdpTcp(params) => Some(params.dst_hst.clone()),
-        SubCommand::RdpTls(identity) => Some(identity.dst_hst.clone()),
+        SubCommand::RdpTcp(params) => Some(params.dst_hst.as_str()),
+        SubCommand::RdpTls(identity) => Some(identity.dst_hst.as_str()),
         _ => None,
     };
 
@@ -154,7 +143,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let signed = JwtSig::new(JwsAlg::RS256, claims.clone()).encode(&private_key)?;
 
     let result = if let GatewayAccessClaims::RoutingClaims(routing_claims) = claims {
-        if let Some(RdpTlsIdentity { jet_public_key, .. }) = routing_claims.identity {
+        if let Some(TlsParams { jet_public_key, .. }) = routing_claims.identity {
             let public_key_str = std::fs::read_to_string(&jet_public_key)?;
             let public_key_pem = public_key_str.parse::<Pem>()?;
             let public_key = PublicKey::from_pem(&public_key_pem)?;
