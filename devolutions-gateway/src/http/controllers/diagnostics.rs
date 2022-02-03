6d1b8dc0609f4016c82d59f00b67687ec81ac083
Benoît CORTIER <benoit.cortier@fried-world.eu>
2022-02-03 17:12:07 -0500

    DGW-10: `jet/diagnostics/clock` route

--- devolutions-gateway/src/http/controllers/diagnostics.rs
+++ devolutions-gateway/src/http/controllers/diagnostics.rs
@@ -5,10 +5,6 @@ use crate::token::JetAccessScope;
 use saphir::prelude::*;
 use std::sync::Arc;
 
-pub struct DiagnosticsController {
-    config: Arc<Config>,
-}
-
 #[derive(Serialize)]
 struct GatewayConfigurationResponse {
     hostname: String,
@@ -24,6 +20,29 @@ impl From<Arc<Config>> for GatewayConfigurationResponse {
             hostname: config.hostname.clone(),
         }
     }
+}
+
+#[derive(Serialize)]
+struct GatewayClockResponse {
+    timestamp_secs: i64,
+    timestamp_millis: i64,
+    timestamp_nanos: i64,
+}
+
+impl GatewayClockResponse {
+    pub fn now() -> Self {
+        use chrono::prelude::*;
+        let utc = Utc::now();
+        Self {
+            timestamp_secs: utc.timestamp(),
+            timestamp_millis: utc.timestamp_millis(),
+            timestamp_nanos: utc.timestamp_nanos(),
+        }
+    }
+}
+
+pub struct DiagnosticsController {
+    config: Arc<Config>,
 }
 
 impl DiagnosticsController {
@@ -46,6 +65,15 @@ impl DiagnosticsController {
     )]
     async fn get_logs(&self) -> Result<File, HttpErrorStatus> {
         get_logs_stub(self).await
+    }
+
+    #[get("/clock")]
+    #[guard(
+        AccessGuard,
+        init_expr = r#"JetTokenType::Scope(JetAccessScope::GatewayDiagnosticsRead)"#
+    )]
+    async fn get_clock(&self) -> Json<GatewayClockResponse> {
+        Json(GatewayClockResponse::now())
     }
 
     #[get("/configuration")]
