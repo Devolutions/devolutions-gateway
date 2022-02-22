e1972846f23fbc444cad2b9f2431cd4bca001d41
Benoît CORTIER <benoit.cortier@fried-world.eu>
2022-02-22 10:34:55 -0500

    transport: integration tests

---
+++ crates/test-utils/src/lib.rs
@@ -0,0 +1,156 @@
+use anyhow::Context as _;
+use proptest::collection::size_range;
+use proptest::prelude::*;
+use tokio::io::{AsyncRead, AsyncWrite};
+use tokio::net::{TcpListener, TcpStream};
+use transport::{Transport, WebSocketStream};
+
+/// For sane Debug display
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+pub struct Payload(pub Vec<u8>);
+
+impl core::fmt::Debug for Payload {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "0x")?;
+        for v in self.0.iter().take(15) {
+            write!(f, "{:X?}", v)?;
+        }
+        write!(f, "..")
+    }
+}
+
+const PAYLOAD_MINIMUM_SIZE: usize = 256;
+const PAYLOAD_MAXIMUM_SIZE: usize = 24 * 256 * 144; // approximately the size of a 144p 24bpp BMP image
+
+prop_compose! {
+    pub fn payload()(data in any_with::<Vec<u8>>(size_range(PAYLOAD_MINIMUM_SIZE..PAYLOAD_MAXIMUM_SIZE).lift())) -> Payload {
+        Payload(data)
+    }
+}
+
+const LARGE_MINIMUM_SIZE: usize = 24 * 1920 * 1080; // approximately the size of a 1080p 24bpp BMP image
+const LARGE_MAXIMUM_SIZE: usize = 24 * 3840 * 2160; // approximately the size of a 4K 24bpp BMP image
+
+prop_compose! {
+    pub fn large_payload()(data in any_with::<Vec<u8>>(size_range(LARGE_MINIMUM_SIZE..LARGE_MAXIMUM_SIZE).lift())) -> Payload {
+        Payload(data)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum TransportKind {
+    Tcp,
+    Ws,
+}
+
+pub fn transport_kind() -> impl Strategy<Value = TransportKind> {
+    prop_oneof![Just(TransportKind::Tcp), Just(TransportKind::Ws)]
+}
+
+impl TransportKind {
+    pub async fn connect(self, port: u16) -> anyhow::Result<Transport> {
+        match self {
+            TransportKind::Ws => ws_connect(port).await,
+            TransportKind::Tcp => tcp_connect(port).await,
+        }
+    }
+
+    pub async fn accept(self, port: u16) -> anyhow::Result<Transport> {
+        match self {
+            TransportKind::Ws => ws_accept(port).await,
+            TransportKind::Tcp => tcp_accept(port).await,
+        }
+    }
+}
+
+pub async fn ws_accept(port: u16) -> anyhow::Result<Transport> {
+    use tokio_tungstenite::accept_async;
+
+    let listener = TcpListener::bind(("127.0.0.1", port)).await?;
+    let (stream, ..) = listener.accept().await?;
+    let ws = accept_async(stream)
+        .await
+        .context("WebSocket handshake failed (accept)")?;
+    let ws = WebSocketStream::new(ws);
+
+    Ok(Transport::new(ws).into_erased())
+}
+
+pub async fn ws_connect(port: u16) -> anyhow::Result<Transport> {
+    use tokio_tungstenite::client_async;
+    use tokio_tungstenite::tungstenite::client::IntoClientRequest as _;
+
+    let stream = TcpStream::connect(("127.0.0.1", port)).await?;
+
+    let req = format!("ws://127.0.0.1:{}", port).into_client_request()?;
+    let (ws, ..) = client_async(req, stream)
+        .await
+        .context("WebSocket handshake failed (connect)")?;
+    let ws = WebSocketStream::new(ws);
+
+    Ok(Transport::new(ws).into_erased())
+}
+
+pub async fn tcp_accept(port: u16) -> anyhow::Result<Transport> {
+    let listener = TcpListener::bind(("127.0.0.1", port)).await?;
+    let (stream, ..) = listener.accept().await?;
+    Ok(Transport::new(stream).into_erased())
+}
+
+pub async fn tcp_connect(port: u16) -> anyhow::Result<Transport> {
+    let stream = TcpStream::connect(("127.0.0.1", port)).await?;
+    Ok(Transport::new(stream).into_erased())
+}
+
+pub async fn write_payload<W: AsyncWrite + Unpin>(writer: &mut W, payload: &[u8]) -> anyhow::Result<()> {
+    use tokio::io::AsyncWriteExt;
+
+    let mut cursor = 0;
+    while cursor < payload.len() {
+        let from = cursor;
+        let to = core::cmp::min(payload.len(), cursor + 9999);
+        writer
+            .write_all(&payload[from..to])
+            .await
+            .context("write_all operation")?;
+        cursor = to;
+    }
+    writer.flush().await.context("Flush operation")?;
+
+    Ok(())
+}
+
+pub async fn read_assert_payload<R: AsyncRead + Unpin>(reader: &mut R, expected_payload: &[u8]) -> anyhow::Result<()> {
+    use tokio::io::AsyncReadExt;
+
+    let mut buf = [0; 5120];
+    let mut current_idx = 0;
+    loop {
+        if current_idx == expected_payload.len() {
+            break;
+        }
+
+        let n = reader.read(&mut buf).await.context("read operation")?;
+        if n == 0 {
+            anyhow::bail!(
+                "Read {current_idx} bytes, but expected exactly {} bytes",
+                expected_payload.len()
+            );
+        }
+
+        let from = current_idx;
+        let to = current_idx + n;
+
+        if to > expected_payload.len() {
+            anyhow::bail!("Received too much bytes");
+        }
+
+        if expected_payload[from..to] != buf[..n] {
+            anyhow::bail!("Received bytes didn't match expected payload ({from}..{to})");
+        }
+
+        current_idx += n;
+    }
+
+    Ok(())
+}
---
+++ crates/transport/tests/forwarding.rs
@@ -0,0 +1,75 @@
+use anyhow::Context as _;
+use futures_util::FutureExt;
+use proptest::prelude::*;
+use test_utils::{payload, read_assert_payload, transport_kind, write_payload, TransportKind};
+use tokio::io::AsyncWriteExt;
+
+async fn client(payload: &[u8], kind: TransportKind, port: u16) -> anyhow::Result<()> {
+    let (mut reader, mut writer) = kind.connect(port).await.context("Connect")?.split();
+    let write_fut = write_payload(&mut writer, payload).map(|res| res.context("Write payload"));
+    let read_fut = read_assert_payload(&mut reader, payload).map(|res| res.context("Assert payload"));
+    tokio::try_join!(write_fut, read_fut)?;
+    writer.shutdown().await.context("Shutdown operation")?;
+    Ok(())
+}
+
+async fn node(
+    port_node: u16,
+    client_kind: TransportKind,
+    port_server: u16,
+    server_kind: TransportKind,
+) -> anyhow::Result<()> {
+    let (mut client_reader, mut client_writer) = client_kind.accept(port_node).await.context("Accept")?.split();
+    let (mut server_reader, mut server_writer) = server_kind.connect(port_server).await.context("Connect")?.split();
+
+    let client_to_server_fut =
+        transport::forward(&mut client_reader, &mut server_writer).map(|res| res.context("Forward to server"));
+    let server_to_client_fut =
+        transport::forward(&mut server_reader, &mut client_writer).map(|res| res.context("Forward to client"));
+
+    tokio::try_join!(client_to_server_fut, server_to_client_fut)?;
+
+    client_writer
+        .shutdown()
+        .await
+        .context("Shutdown operation on client_writer")?;
+    server_writer
+        .shutdown()
+        .await
+        .context("Shutdown operation on server_writer")?;
+
+    Ok(())
+}
+
+async fn server(payload: &[u8], kind: TransportKind, port: u16) -> anyhow::Result<()> {
+    let (mut reader, mut writer) = kind.accept(port).await.context("Accept")?.split();
+    let write_fut = write_payload(&mut writer, payload).map(|res| res.context("Write payload"));
+    let read_fut = read_assert_payload(&mut reader, payload).map(|res| res.context("Assert payload"));
+    tokio::try_join!(write_fut, read_fut)?;
+    writer.shutdown().await.context("Shutdown operation")?;
+    Ok(())
+}
+
+#[test]
+fn three_points() {
+    let port_node = portpicker::pick_unused_port().expect("No available port");
+    let port_server = portpicker::pick_unused_port().expect("No available port");
+
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    proptest!(ProptestConfig::with_cases(10), |(
+        payload in payload().no_shrink(),
+        client_to_node_kind in transport_kind(),
+        node_to_server_kind in transport_kind(),
+    )| {
+        rt.block_on(async {
+            let server_fut = server(&payload.0, node_to_server_kind, port_server).map(|res| res.context("Server"));
+            let node_fut = node(port_node, client_to_node_kind, port_server, node_to_server_kind).map(|res| res.context("Node"));
+            let client_fut = client(&payload.0, client_to_node_kind, port_node).map(|res| res.context("Client"));
+            tokio::try_join!(server_fut, node_fut, client_fut).unwrap();
+        });
+    })
+}
