ebe58e431b6fde694bddf26d4302118791675fa7
Benoît CORTIER <benoit.cortier@fried-world.eu>
2022-03-15 16:37:57 -0400

    gateway: add integration tests for token security

--- crates/devolutions-gateway-generators/src/lib.rs
+++ crates/devolutions-gateway-generators/src/lib.rs
@@ -1,7 +1,10 @@
-use devolutions_gateway::token::ApplicationProtocol;
+use devolutions_gateway::token::{self, ApplicationProtocol};
 use devolutions_gateway::utils::TargetAddr;
 use devolutions_gateway::{ConnectionModeDetails, GatewaySessionInfo};
+use proptest::collection::vec;
+use proptest::option;
 use proptest::prelude::*;
+use serde::Serialize;
 
 pub fn uuid_str() -> impl Strategy<Value = String> {
     "[[:digit:]]{8}-([[:digit:]]{4}-){3}[[:digit:]]{12}".no_shrink()
@@ -9,6 +12,18 @@ pub fn uuid_str() -> impl Strategy<Value = String> {
 
 pub fn uuid_typed() -> impl Strategy<Value = uuid::Uuid> {
     uuid_str().prop_map(|id| uuid::Uuid::parse_str(&id).unwrap())
+}
+
+pub fn token_content_type() -> impl Strategy<Value = token::ContentType> {
+    prop_oneof![
+        Just(token::ContentType::Association),
+        Just(token::ContentType::Scope),
+        Just(token::ContentType::Bridge),
+        Just(token::ContentType::Jmux),
+        Just(token::ContentType::Kdc),
+        Just(token::ContentType::Jrl),
+    ]
+    .no_shrink()
 }
 
 pub fn application_protocol() -> impl Strategy<Value = ApplicationProtocol> {
@@ -26,6 +41,128 @@ pub fn application_protocol() -> impl Strategy<Value = ApplicationProtocol> {
 
 pub fn target_addr() -> impl Strategy<Value = TargetAddr> {
     "[a-z]{1,10}\\.[a-z]{1,5}:[0-9]{3,4}".prop_map(|addr| TargetAddr::parse(&addr, None).unwrap())
+}
+
+pub fn host() -> impl Strategy<Value = String> {
+    "[a-z]{1,10}\\.[a-z]{1,5}(:[0-9]{3,4})?"
+}
+
+pub fn alternate_hosts() -> impl Strategy<Value = Vec<String>> {
+    vec(host(), 0..4)
+}
+
+pub fn host_with_port() -> impl Strategy<Value = String> {
+    "[a-z]{1,10}\\.[a-z]{1,5}:[0-9]{3,4}"
+}
+
+pub fn alternate_hosts_with_ports() -> impl Strategy<Value = Vec<String>> {
+    vec(host_with_port(), 0..4)
+}
+
+#[derive(Debug, Serialize, Clone)]
+pub struct CredsClaims {
+    pub prx_usr: String,
+    pub prx_pwd: String,
+    pub dst_usr: String,
+    pub dst_pwd: String,
+}
+
+pub fn creds_claims() -> impl Strategy<Value = CredsClaims> {
+    (".*", ".*", ".*", ".*")
+        .prop_map(|(prx_usr, prx_pwd, dst_usr, dst_pwd)| CredsClaims {
+            prx_usr,
+            prx_pwd,
+            dst_usr,
+            dst_pwd,
+        })
+        .no_shrink()
+}
+
+#[derive(Debug, Serialize, Clone)]
+#[serde(rename_all = "kebab-case")]
+#[serde(tag = "jet_cm")]
+pub enum ConnectionMode {
+    Rdv,
+    Fwd {
+        /// Destination Host
+        dst_hst: String,
+        /// Alternate Destination Hosts
+        dst_alt: Vec<String>,
+        #[serde(flatten)]
+        creds: Option<CredsClaims>,
+    },
+}
+
+pub fn jet_ap_and_jet_cm() -> impl Strategy<Value = (ApplicationProtocol, ConnectionMode)> {
+    prop_oneof![
+        (application_protocol(), Just(ConnectionMode::Rdv)),
+        application_protocol().prop_flat_map(|jet_ap| {
+            if jet_ap.known_default_port().is_some() {
+                (
+                    Just(jet_ap),
+                    (host(), alternate_hosts(), option::of(creds_claims())).prop_map(|(dst_hst, dst_alt, creds)| {
+                        ConnectionMode::Fwd {
+                            dst_hst,
+                            dst_alt,
+                            creds,
+                        }
+                    }),
+                )
+                    .boxed()
+            } else {
+                (
+                    Just(jet_ap),
+                    (
+                        host_with_port(),
+                        alternate_hosts_with_ports(),
+                        option::of(creds_claims()),
+                    )
+                        .prop_map(|(dst_hst, dst_alt, creds)| ConnectionMode::Fwd {
+                            dst_hst,
+                            dst_alt,
+                            creds,
+                        }),
+                )
+                    .boxed()
+            }
+        }),
+    ]
+    .no_shrink()
+}
+
+#[derive(Debug, Serialize, Clone)]
+pub struct AssociationClaims {
+    pub jet_aid: uuid::Uuid,
+    pub jet_ap: ApplicationProtocol,
+    #[serde(flatten)]
+    pub jet_cm: ConnectionMode,
+    pub jet_rec: bool,
+    pub jet_flt: bool,
+    pub nbf: i64,
+    pub exp: i64,
+    pub jti: uuid::Uuid,
+}
+
+pub fn any_association_claims(now: i64) -> impl Strategy<Value = AssociationClaims> {
+    (
+        uuid_typed(),
+        jet_ap_and_jet_cm(),
+        any::<bool>(),
+        any::<bool>(),
+        uuid_typed(),
+    )
+        .prop_map(
+            move |(jet_aid, (jet_ap, jet_cm), jet_rec, jet_flt, jti)| AssociationClaims {
+                jet_aid,
+                jet_ap,
+                jet_cm,
+                jet_rec,
+                jet_flt,
+                jti,
+                nbf: now,
+                exp: now + 1000,
+            },
+        )
 }
 
 pub fn session_info_fwd_only() -> impl Strategy<Value = GatewaySessionInfo> {
---
+++ devolutions-gateway/tests/token_security.rs
@@ -0,0 +1,232 @@
+use anyhow::Context as _;
+use devolutions_gateway::token::{new_token_cache, ApplicationProtocol, JrlTokenClaims};
+use devolutions_gateway_generators::*;
+use parking_lot::Mutex;
+use picky::jose::jwe;
+use picky::jose::jws::JwsAlg;
+use picky::jose::jwt::CheckedJwtSig;
+use picky::key::{PrivateKey, PublicKey};
+use proptest::collection::vec;
+use proptest::option;
+use proptest::prelude::*;
+use std::net::IpAddr;
+
+const KEY: &str = r#"-----BEGIN PRIVATE KEY-----
+MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDkrPiL/5dmGIT5
+/KuC3H/jIjeLoLoddsLhAlikO5JQQo3Zs71GwT4Wd2z8WLMe0lVZu/Jr2S28p0M8
+F3Lnz4IgzjocQomFgucFWWQRyD03ZE2BHfEeelFsp+/4GZaM6lKZauYlIMtjR1vD
+lflgvxNTr0iaii4JR9K3IKCunCRy1HQYPcZ9waNtlG5xXtW9Uf1tLWPJpP/3I5HL
+M85JPBv4r286vpeUlfQIa/NB4g5w6KZ6MfEAIU4KeEQpeLAyyYvwUzPR2uQZ4y4I
+4Nj84dWYB1cMTlSGugvSgOFKYit1nwLGeA7EevVYPbILRfSMBU/+avGNJJ8HCaaq
+FIyY42W9AgMBAAECggEBAImsGXcvydaNrIFUvW1rkxML5qUJfwN+HJWa9ALsWoo3
+h28p5ypR7S9ZdyP1wuErgHcl0C1d80tA6BmlhGhLZeyaPCIHbQQUa0GtL7IE+9X9
+bSvu+tt+iMcB1FdqEFmGOXRkB2sS82Ax9e0qvZihcOFRBkUEK/MqapIV8qctGkSG
+wIE6yn5LHRls/fJU8BJeeqJmYpuWljipwTkp9hQ7SdRYFLNjwjlz/b0hjmgFs5QZ
+LUNMyTHdHtXQHNsf/GayRUAKf5wzN/jru+nK6lMob2Ehfx9/RAfgaDHzy5BNFMj0
+i9+sAycgIW1HpTuDvSEs3qP26NeQ82GbJzATmdAKa4ECgYEA9Vti0YG+eXJI3vdS
+uXInU0i1SY4aEG397OlGMwh0yQnp2KGruLZGkTvqxG/Adj1ObDyjFH9XUhMrd0za
+Nk/VJFybWafljUPcrfyPAVLQLjsBfMg3Y34sTF6QjUnhg49X2jfvy9QpC5altCtA
+46/KVAGREnQJ3wMjfGGIFP8BUZsCgYEA7phYE/cYyWg7a/o8eKOFGqs11ojSqG3y
+0OE7kvW2ugUuy3ex+kr19Q/8pOWEc7M1UEV8gmc11xgB70EhIFt9Jq379H0X4ahS
++mgLiPzKAdNCRPpkxwwN9HxFDgGWoYcgMplhoAmg9lWSDuE1Exy8iu5inMWuF4MT
+/jG+cLnUZ4cCgYAfMIXIUjDvaUrAJTp73noHSUfaWNkRW5oa4rCMzjdiUwNKCYs1
+yN4BmldGr1oM7dApTDAC7AkiotM0sC1RGCblH2yUIha5NXY5G9Dl/yv9pHyU6zK3
+UBO7hY3kmA611aP6VoACLi8ljPn1hEYUa4VR1n0llmCm29RH/HH7EUuOnwKBgExH
+OCFp5eq+AAFNRvfqjysvgU7M/0wJmo9c8obRN1HRRlyWL7gtLuTh74toNSgoKus2
+y8+E35mce0HaOJT3qtMq3FoVhAUIoz6a9NUevBZJS+5xfraEDBIViJ4ps9aANLL4
+hlV7vpICWWeYaDdsAHsKK0yjhjzOEx45GQFA578RAoGBAOB42BG53tL0G9pPeJPt
+S2LM6vQKeYx+gXTk6F335UTiiC8t0CgNNQUkW105P/SdpCTTKojAsOPMKOF7z4mL
+lj/bWmNq7xu9uVOcBKrboVFGO/n6FXyWZxHPOTdjTkpe8kvvmSwl2iaTNllvSr46
+Z/fDKMxHxeXla54kfV+HiGkH
+-----END PRIVATE KEY-----"#;
+
+#[derive(Debug, Clone)]
+struct RevocableItem {
+    claim_to_revoke: Option<(String, serde_json::Value)>,
+    claims: serde_json::Value,
+    token: String,
+}
+
+fn revocable_item<'a>(
+    now: i64,
+    pub_key: &'a PublicKey,
+    priv_key: &'a PrivateKey,
+) -> impl Strategy<Value = RevocableItem> + 'a {
+    any_association_claims(now)
+        .prop_flat_map(move |claims| {
+            let mut token = CheckedJwtSig::new(JwsAlg::RS256, &claims);
+            token.header.cty = Some("ASSOCIATION".to_owned());
+            let token = token.encode(&priv_key).unwrap();
+            let token = if matches!(claims.jet_cm, ConnectionMode::Fwd { creds: Some(_), .. }) {
+                jwe::Jwe::new(jwe::JweAlg::RsaOaep256, jwe::JweEnc::Aes256Gcm, token.into_bytes())
+                    .encode(&pub_key)
+                    .unwrap()
+            } else {
+                token
+            };
+
+            let claims = serde_json::to_value(&claims).unwrap();
+            let nb_claims = claims.as_object().unwrap().len();
+            (Just(claims), option::of(0..nb_claims), Just(token))
+        })
+        .prop_map(|(claims, idx_to_revoke, token)| {
+            let claim_to_revoke = idx_to_revoke
+                .and_then(|idx| claims.as_object().unwrap().iter().nth(idx))
+                .map(|(k, v)| (k.clone(), v.clone()));
+
+            RevocableItem {
+                claim_to_revoke,
+                claims,
+                token,
+            }
+        })
+        .no_shrink()
+}
+
+#[test]
+fn revocation_list() {
+    let priv_key = PrivateKey::from_pem_str(KEY).unwrap();
+    let pub_key = priv_key.to_public_key();
+    let source_ip = IpAddr::from([13u8, 12u8, 11u8, 10u8]);
+    let now = chrono::Utc::now().timestamp();
+
+    let test_impl = |items: Vec<RevocableItem>| -> anyhow::Result<()> {
+        // Make sure all tokens are valid before any revocation
+
+        let empty_jrl = Mutex::new(JrlTokenClaims::default());
+        let token_cache = new_token_cache();
+
+        for (idx, item) in items.iter().enumerate() {
+            devolutions_gateway::token::validate_token(
+                &item.token,
+                source_ip,
+                &pub_key,
+                Some(&priv_key),
+                &token_cache,
+                &empty_jrl,
+            )
+            .with_context(|| format!("Item n°{idx} validation failed"))?;
+        }
+
+        // Revoke claims
+
+        let updated_jrl = {
+            let mut claims = JrlTokenClaims::default();
+
+            for item in &items {
+                let (k, v) = if let Some((k, v)) = &item.claim_to_revoke {
+                    (k.clone(), v.clone())
+                } else {
+                    continue;
+                };
+
+                let bucket = claims.jrl.entry(k).or_insert_with(Vec::new);
+
+                bucket.push(v);
+            }
+
+            claims
+        };
+
+        // Update item list and keep track of which token is revoked
+
+        let items: Vec<_> = items
+            .into_iter()
+            .map(|item| {
+                if item.claim_to_revoke.is_some() {
+                    (item, true)
+                } else {
+                    let is_revoked = updated_jrl
+                        .jrl
+                        .iter()
+                        .any(|(key, revoked_values)| match item.claims.get(key) {
+                            Some(token_value) if revoked_values.contains(token_value) => true,
+                            _ => false,
+                        });
+                    (item, is_revoked)
+                }
+            })
+            .collect();
+
+        // Validate that only revoked tokens are refused
+
+        let updated_jrl = Mutex::new(updated_jrl);
+        let token_cache = new_token_cache();
+
+        for (idx, (item, is_revoked)) in items.into_iter().enumerate() {
+            let res = devolutions_gateway::token::validate_token(
+                &item.token,
+                source_ip,
+                &pub_key,
+                Some(&priv_key),
+                &token_cache,
+                &updated_jrl,
+            );
+
+            if is_revoked {
+                let e = res
+                    .err()
+                    .with_context(|| format!("Item n°{idx} validation should have failed, but it didn't"))?;
+                assert!(e.to_string().contains("Received a token containing a revoked value"));
+            } else {
+                res.with_context(|| format!("Item n°{idx} validation failed, but it wasn't expected to"))?;
+            }
+        }
+
+        Ok(())
+    };
+
+    proptest!(ProptestConfig::with_cases(16), |(items in vec(revocable_item(now, &pub_key, &priv_key), 1..5))| {
+        test_impl(items).map_err(|e| TestCaseError::fail(format!("{:#}", e)))?;
+    });
+}
+
+#[test]
+fn token_cache() {
+    let jrl = Mutex::new(JrlTokenClaims::default());
+    let priv_key = PrivateKey::from_pem_str(KEY).unwrap();
+    let pub_key = priv_key.to_public_key();
+    let source_ip = IpAddr::from([13u8, 12u8, 11u8, 10u8]);
+    let source_ip_2 = IpAddr::from([15u8, 12u8, 11u8, 10u8]);
+    let now = chrono::Utc::now().timestamp();
+
+    let test_impl = |same_ip: bool, claims: AssociationClaims| -> anyhow::Result<()> {
+        let mut token = CheckedJwtSig::new(JwsAlg::RS256, &claims);
+        token.header.cty = Some("ASSOCIATION".to_owned());
+        let token = token.encode(&priv_key)?;
+        let token = if matches!(claims.jet_cm, ConnectionMode::Fwd { creds: Some(_), .. }) {
+            jwe::Jwe::new(jwe::JweAlg::RsaOaep256, jwe::JweEnc::Aes256Gcm, token.into_bytes()).encode(&pub_key)?
+        } else {
+            token
+        };
+
+        let token_cache = new_token_cache();
+
+        devolutions_gateway::token::validate_token(&token, source_ip, &pub_key, Some(&priv_key), &token_cache, &jrl)?;
+
+        let ip_when_reusing = if same_ip { source_ip } else { source_ip_2 };
+
+        let res = devolutions_gateway::token::validate_token(
+            &token,
+            ip_when_reusing,
+            &pub_key,
+            Some(&priv_key),
+            &token_cache,
+            &jrl,
+        );
+
+        if same_ip && claims.jet_ap == ApplicationProtocol::Rdp {
+            // RDP association tokens can be re-used if source IP is identical
+            res?;
+        } else {
+            let e = res.err().context("validation should have failed")?;
+            assert!(e.to_string().contains("Received identical token twice"));
+        }
+
+        Ok(())
+    };
+
+    proptest!(ProptestConfig::with_cases(32), |(same_ip in any::<bool>(), claims in any_association_claims(now).no_shrink())| {
+        test_impl(same_ip, claims).map_err(|e| TestCaseError::fail(format!("{:#}", e)))?;
+    });
+}
