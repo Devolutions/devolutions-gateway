b20fa4edd123f2da2e006c732cef6f1a18882759
Benoît CORTIER <bcortier@pm.me>
2022-04-05 16:53:09 -0400

    feat(tokengen): support latest formats

--- tools/tokengen/src/main.rs
+++ tools/tokengen/src/main.rs
@@ -1,15 +1,142 @@
 use clap::Parser;
-use humantime::parse_duration;
 use picky::jose::jwe::{Jwe, JweAlg, JweEnc};
 use picky::jose::jws::JwsAlg;
-use picky::jose::jwt::JwtSig;
+use picky::jose::jwt::CheckedJwtSig;
 use picky::key::{PrivateKey, PublicKey};
 use picky::pem::Pem;
 use serde::Serialize;
+use std::collections::HashMap;
 use std::error::Error;
 use std::path::PathBuf;
 use std::time::SystemTime;
 use uuid::Uuid;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let app = App::parse();
+
+    let private_key_str = std::fs::read_to_string(&app.provider_private_key)?;
+    let private_key_pem = private_key_str.parse::<Pem>()?;
+    let private_key = PrivateKey::from_pem(&private_key_pem)?;
+
+    let validity_duration = humantime::parse_duration(&app.validity_duration)?;
+    let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?;
+    let nbf = i64::try_from(now.as_secs()).unwrap();
+    let exp = i64::try_from((now + validity_duration).as_secs()).unwrap();
+
+    let jti = Uuid::new_v4();
+
+    let (cty, claims) = match app.subcmd {
+        SubCommand::RdpTcp(params) => {
+            let claims = AssociationClaims {
+                exp,
+                nbf,
+                jti,
+                dst_hst: Some(&params.dst_hst),
+                jet_cm: "fwd",
+                jet_ap: "rdp",
+                jet_aid: Uuid::new_v4(),
+                creds: None,
+            };
+            ("ASSOCIATION", serde_json::to_value(&claims).unwrap())
+        }
+        SubCommand::RdpTls(params) => {
+            let claims = AssociationClaims {
+                exp,
+                nbf,
+                jti,
+                dst_hst: Some(&params.dst_hst),
+                jet_cm: "fwd",
+                jet_ap: "rdp",
+                jet_aid: Uuid::new_v4(),
+                creds: Some(CredsClaims {
+                    prx_usr: &params.prx_usr,
+                    prx_pwd: &params.prx_pwd,
+                    dst_usr: &params.dst_usr,
+                    dst_pwd: &params.dst_pwd,
+                }),
+            };
+            ("ASSOCIATION", serde_json::to_value(&claims).unwrap())
+        }
+        SubCommand::RdpTcpRendezvous => {
+            let claims = AssociationClaims {
+                exp,
+                nbf,
+                jti,
+                dst_hst: None,
+                jet_cm: "rdv",
+                jet_ap: "rdp",
+                jet_aid: Uuid::new_v4(),
+                creds: None,
+            };
+            ("ASSOCIATION", serde_json::to_value(&claims).unwrap())
+        }
+        SubCommand::Scope(params) => {
+            let claims = ScopeClaims {
+                exp,
+                nbf,
+                jti,
+                scope: &params.scope,
+            };
+            ("SCOPE", serde_json::to_value(&claims).unwrap())
+        }
+        SubCommand::Jmux(params) => {
+            let claims = JmuxClaims {
+                dst_hst: &params.dst_hst,
+                dst_addl: params.dst_addl.iter().map(|o| o.as_str()).collect(),
+                exp,
+                nbf,
+                jti,
+            };
+            ("JMUX", serde_json::to_value(&claims).unwrap())
+        }
+        SubCommand::Kdc(params) => {
+            let claims = KdcClaims {
+                exp,
+                nbf,
+                krb_realm: &params.krb_realm,
+                krb_kdc: &params.krb_kdc,
+                jti,
+            };
+            ("KDC", serde_json::to_value(&claims).unwrap())
+        }
+        SubCommand::Jrl(params) => {
+            let claims = JrlClaims {
+                jti,
+                iat: nbf,
+                jrl: {
+                    let mut jrl = HashMap::new();
+                    jrl.insert(
+                        "jti",
+                        params
+                            .jti
+                            .into_iter()
+                            .map(|id| serde_json::Value::String(id.to_string()))
+                            .collect(),
+                    );
+                    jrl
+                },
+            };
+            ("JRL", serde_json::to_value(&claims).unwrap())
+        }
+    };
+
+    let jwt_sig = CheckedJwtSig::new_with_cty(JwsAlg::RS256, cty, claims);
+    let signed = jwt_sig.encode(&private_key)?;
+
+    let result = if let Some(delegation_public_key) = app.delegation_public_key {
+        let public_key = std::fs::read_to_string(&delegation_public_key)?;
+        let public_key = PublicKey::from_pem_str(&public_key)?;
+        Jwe::new(JweAlg::RsaOaep256, JweEnc::Aes256Gcm, signed.into_bytes()).encode(&public_key)?
+    } else {
+        signed
+    };
+
+    println!("{result}");
+
+    Ok(())
+}
+
+// --- CLI App --- //
 
 #[derive(Parser)]
 struct App {
@@ -17,13 +144,12 @@ struct App {
     validity_duration: String,
     #[clap(long)]
     provider_private_key: PathBuf,
+    #[clap(long)]
+    delegation_public_key: Option<PathBuf>,
     #[clap(subcommand)]
     subcmd: SubCommand,
 }
 
-// clippy: All enumeration variants that are prefixed with `Rdp`; this produces the clippy error.
-// It will be marked as allowed as we may add new non-RDP related protocols in the future
-#[allow(clippy::enum_variant_names)]
 #[derive(Parser)]
 enum SubCommand {
     RdpTcp(TcpParams),
@@ -32,6 +158,7 @@ enum SubCommand {
     Scope(ScopeParams),
     Jmux(JmuxParams),
     Kdc(KdcParams),
+    Jrl(JrlParams),
 }
 
 #[derive(Parser)]
@@ -40,15 +167,10 @@ struct TcpParams {
     dst_hst: String,
 }
 
-#[derive(Clone, Parser, Serialize)]
+#[derive(Parser)]
 struct TlsParams {
-    #[serde(skip_serializing)]
-    #[clap(long)]
-    jet_public_key: PathBuf,
-    #[serde(skip_serializing)]
     #[clap(long)]
     dst_hst: String,
-
     #[clap(long)]
     prx_usr: String,
     #[clap(long)]
@@ -79,33 +201,41 @@ struct KdcParams {
     krb_kdc: String,
 }
 
-#[derive(Clone, Serialize)]
-#[serde(tag = "type")]
-#[serde(rename_all = "kebab-case")]
-enum GatewayAccessClaims<'a> {
-    Association(AssociationClaims<'a>),
-    Scope(ScopeClaims),
-    Jmux(JmuxClaims<'a>),
-    Kdc(KdcClaims),
+#[derive(Parser)]
+struct JrlParams {
+    #[clap(long)]
+    jti: Vec<Uuid>,
 }
+
+// --- claims --- //
 
 #[derive(Clone, Serialize)]
 struct AssociationClaims<'a> {
     exp: i64,
     nbf: i64,
+    jti: Uuid,
     jet_cm: &'a str,
     jet_ap: &'a str,
     jet_aid: Uuid,
     dst_hst: Option<&'a str>,
     #[serde(flatten)]
-    identity: Option<TlsParams>,
+    creds: Option<CredsClaims<'a>>,
 }
 
 #[derive(Clone, Serialize)]
-struct ScopeClaims {
+pub struct CredsClaims<'a> {
+    prx_usr: &'a str,
+    prx_pwd: &'a str,
+    dst_usr: &'a str,
+    dst_pwd: &'a str,
+}
+
+#[derive(Clone, Serialize)]
+struct ScopeClaims<'a> {
     exp: i64,
     nbf: i64,
-    scope: String,
+    jti: Uuid,
+    scope: &'a str,
 }
 
 #[derive(Clone, Serialize)]
@@ -118,95 +248,17 @@ struct JmuxClaims<'a> {
 }
 
 #[derive(Clone, Serialize)]
-struct KdcClaims {
-    krb_realm: String,
-    krb_kdc: String,
+struct KdcClaims<'a> {
+    krb_realm: &'a str,
+    krb_kdc: &'a str,
     exp: i64,
     nbf: i64,
     jti: Uuid,
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let app = App::parse();
-
-    let private_key_str = std::fs::read_to_string(&app.provider_private_key)?;
-    let private_key_pem = private_key_str.parse::<Pem>()?;
-    let private_key = PrivateKey::from_pem(&private_key_pem)?;
-
-    let validity_duration = parse_duration(&app.validity_duration)?;
-    let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?;
-
-    let exp = (now + validity_duration).as_secs();
-
-    let jet_cm = match app.subcmd {
-        SubCommand::RdpTcpRendezvous => "rdv",
-        _ => "fwd",
-    };
-
-    let jet_ap = "rdp";
-
-    let jet_aid = Uuid::new_v4();
-
-    let identity = match &app.subcmd {
-        SubCommand::RdpTls(identity) => Some(identity.clone()),
-        _ => None,
-    };
-
-    let dst_hst = match &app.subcmd {
-        SubCommand::RdpTcp(params) => Some(params.dst_hst.as_str()),
-        SubCommand::RdpTls(identity) => Some(identity.dst_hst.as_str()),
-        _ => None,
-    };
-
-    let claims = match &app.subcmd {
-        SubCommand::Scope(params) => GatewayAccessClaims::Scope(ScopeClaims {
-            exp: exp as i64,
-            nbf: now.as_secs() as i64,
-            scope: params.scope.clone(),
-        }),
-        SubCommand::Jmux(params) => GatewayAccessClaims::Jmux(JmuxClaims {
-            dst_hst: &params.dst_hst,
-            dst_addl: params.dst_addl.iter().map(|o| o.as_str()).collect(),
-            exp: exp as i64,
-            nbf: now.as_secs() as i64,
-            jti: Uuid::new_v4(),
-        }),
-        SubCommand::RdpTcp(_) | SubCommand::RdpTls(_) | SubCommand::RdpTcpRendezvous => {
-            GatewayAccessClaims::Association(AssociationClaims {
-                exp: exp as i64,
-                nbf: now.as_secs() as i64,
-                dst_hst,
-                jet_cm,
-                jet_ap,
-                jet_aid,
-                identity,
-            })
-        }
-        SubCommand::Kdc(params) => GatewayAccessClaims::Kdc(KdcClaims {
-            exp: exp as i64,
-            nbf: now.as_secs() as i64,
-            krb_realm: params.krb_realm.clone(),
-            krb_kdc: params.krb_kdc.clone(),
-            jti: Uuid::new_v4(),
-        }),
-    };
-
-    let signed = JwtSig::new(JwsAlg::RS256, claims.clone()).encode(&private_key)?;
-
-    let result = if let GatewayAccessClaims::Association(routing_claims) = claims {
-        if let Some(TlsParams { jet_public_key, .. }) = routing_claims.identity {
-            let public_key_str = std::fs::read_to_string(&jet_public_key)?;
-            let public_key_pem = public_key_str.parse::<Pem>()?;
-            let public_key = PublicKey::from_pem(&public_key_pem)?;
-            Jwe::new(JweAlg::RsaOaep256, JweEnc::Aes256Gcm, signed.into_bytes()).encode(&public_key)?
-        } else {
-            signed
-        }
-    } else {
-        signed
-    };
-
-    println!("{}", result);
-
-    Ok(())
+#[derive(Clone, Serialize)]
+struct JrlClaims<'a> {
+    jti: Uuid,
+    iat: i64,
+    jrl: HashMap<&'a str, Vec<serde_json::Value>>,
 }
