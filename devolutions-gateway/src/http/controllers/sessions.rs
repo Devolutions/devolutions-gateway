6f5cc8f7c560b9b9cfde8dd0b3541ec972963f0e
Benoît CORTIER <bcortier@proton.me>
2022-06-13 15:57:19 -0400

    refactor!(dgw): remove unused `/jet/sessions/count` route

--- devolutions-gateway/src/http/controllers/sessions.rs
+++ devolutions-gateway/src/http/controllers/sessions.rs
@@ -3,7 +3,7 @@ use crate::http::HttpErrorStatus;
 use crate::token::JetAccessScope;
 use crate::{GatewaySessionInfo, SESSIONS_IN_PROGRESS};
 use saphir::controller::Controller;
-use saphir::http::{Method, StatusCode};
+use saphir::http::Method;
 use saphir::macros::controller;
 use saphir::prelude::Json;
 
@@ -11,22 +11,11 @@ pub struct SessionsController;
 
 #[controller(name = "jet/sessions")]
 impl SessionsController {
-    #[get("/count")]
-    #[guard(AccessGuard, init_expr = r#"TokenType::Scope(JetAccessScope::GatewaySessionsRead)"#)]
-    async fn get_count(&self) -> (StatusCode, String) {
-        get_count_stub().await
-    }
-
     #[get("/")]
     #[guard(AccessGuard, init_expr = r#"TokenType::Scope(JetAccessScope::GatewaySessionsRead)"#)]
     async fn get_sessions(&self) -> Result<Json<Vec<GatewaySessionInfo>>, HttpErrorStatus> {
         get_sessions_stub().await
     }
-}
-
-async fn get_count_stub() -> (StatusCode, String) {
-    let sessions = SESSIONS_IN_PROGRESS.read().await;
-    (StatusCode::OK, sessions.len().to_string())
 }
 
 async fn get_sessions_stub() -> Result<Json<Vec<GatewaySessionInfo>>, HttpErrorStatus> {
@@ -43,12 +32,6 @@ pub struct LegacySessionsController;
 
 #[controller(name = "sessions")]
 impl LegacySessionsController {
-    #[get("/count")]
-    #[guard(AccessGuard, init_expr = r#"TokenType::Scope(JetAccessScope::GatewaySessionsRead)"#)]
-    async fn get_count(&self) -> (StatusCode, String) {
-        get_count_stub().await
-    }
-
     #[get("/")]
     #[guard(AccessGuard, init_expr = r#"TokenType::Scope(JetAccessScope::GatewaySessionsRead)"#)]
     async fn get_sessions(&self) -> Result<Json<Vec<GatewaySessionInfo>>, HttpErrorStatus> {
