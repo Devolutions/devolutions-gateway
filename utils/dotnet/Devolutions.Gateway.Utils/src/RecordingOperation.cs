02eee02046420876a3d21968bb43e28a47f3ed25
François Dubois <fdubois@devolutions.net>
2023-05-12 10:24:34 -0400

    feat(dotnet-utils): recording token claims (#436)
    Issue: DPS-7341

---
+++ utils/dotnet/Devolutions.Gateway.Utils/src/RecordingOperation.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Devolutions.Gateway.Utils;
+
+[JsonConverter(typeof(RecordingOperationJsonConverter))]
+public struct RecordingOperation
+{
+    public string Value { get; internal set; }
+
+    internal RecordingOperation(string value)
+    {
+        Value = value;
+    }
+
+    public static RecordingOperation Push = new RecordingOperation("push");
+    public static RecordingOperation Pull = new RecordingOperation("pull");
+
+    public override string? ToString()
+    {
+        return this.Value;
+    }
+}
