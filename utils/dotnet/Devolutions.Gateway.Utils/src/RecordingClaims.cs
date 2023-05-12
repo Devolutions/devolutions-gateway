238d1a59419af0a1fef71f90c19db3c7eef7d4b1
François Dubois <fdubois@devolutions.net>
2023-05-11 22:05:07 -0400

    [DPS-7341] Ajout de token pour le recording

---
+++ utils/dotnet/Devolutions.Gateway.Utils/src/RecordingClaims.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Devolutions.Gateway.Utils;
+
+public class RecordingClaims : IGatewayClaims
+{
+    [JsonPropertyName("jet_aid")]
+    public Guid SessionId { get; set; }
+    [JsonPropertyName("jet_rop")]
+    public RecordingOperation RecordingOperation { get; set; }
+    [JsonPropertyName("jet_gw_id")]
+    public Guid ScopeGatewayId { get; set; }
+
+    public RecordingClaims(
+        Guid scopeGatewayId,
+        Guid sessionId, 
+        RecordingOperation recordingOperation)
+    {
+        this.ScopeGatewayId = scopeGatewayId;
+        this.SessionId = sessionId;
+        this.RecordingOperation = recordingOperation;
+    }
+
+    public string GetContentType()
+    {
+        return "JREC";
+    }
+}
---
+++ utils/dotnet/Devolutions.Gateway.Utils/src/RecordingOperationJsonConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Devolutions.Gateway.Utils;
+
+public class RecordingOperationJsonConverter : JsonConverter<RecordingOperation>
+{
+    public override RecordingOperation Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options) => new RecordingOperation(reader.GetString()!);
+
+    public override void Write(
+        Utf8JsonWriter writer,
+        RecordingOperation recordingOperation,
+        JsonSerializerOptions options) => writer.WriteStringValue(recordingOperation.ToString());
+}
