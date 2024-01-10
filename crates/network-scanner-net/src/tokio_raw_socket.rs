549b7d4b7c410a6d56ebd3cbec949f35e3566281
irving ou <jou@devolutions.net>
2024-01-10 10:41:28 -0500

    refactor, should not use tokio AsyncRead and AsyncWrite interface

--- crates/network-scanner-net/src/tokio_raw_socket.rs
+++ crates/network-scanner-net/src/tokio_raw_socket.rs
@@ -1,122 +1,125 @@
-use std::{
-    io::{Read, Write},
-    net::Ipv4Addr,
-};
+use std::{mem::MaybeUninit, sync::Arc, usize};
 
 use socket2::SockAddr;
 use std::result::Result::Ok;
-use tokio::io::{AsyncRead, AsyncWrite};
+use tokio::sync::Mutex;
 
-pub struct TokioRawSocketStream {
-    socket: socket2::Socket,
+/// A wrapper on raw socket that can be used with async tokio runtime
+/// This currently only throws the blocking calls on a blocking thread pool
+/// In the future, this should be replaced with a non-blocking implementation that takes advantage of OS specific async APIs
+/// We are seeking to match the function signatures of socket2::Socket, but in async form
+pub struct TokioRawSocket {
+    socket: Arc<Mutex<socket2::Socket>>,
 }
 
-impl TokioRawSocketStream {
-    pub async fn connect(ip: impl Into<Ipv4Addr>) -> std::io::Result<TokioRawSocketStream> {
-        let socket_addr = std::net::SocketAddrV4::new(ip.into(), 0); // raw sockets don't need a port
-        let socket_addr = SockAddr::from(socket_addr);
-        tracing::trace!("Connecting to {:?}", &socket_addr);
-        let socket = tokio::task::spawn_blocking(move || {
-            let socket = socket2::Socket::new(
-                socket2::Domain::IPV4,
-                socket2::Type::RAW,
-                Some(socket2::Protocol::ICMPV4),
-            )
-            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+impl TokioRawSocket {
+    pub fn new(
+        domain: socket2::Domain,
+        ty: socket2::Type,
+        protocol: Option<socket2::Protocol>,
+    ) -> std::io::Result<TokioRawSocket> {
 
-            socket
-                .connect(&socket_addr)
-                .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        let socket = socket2::Socket::new(domain, ty, protocol)?;
+        let socket = Arc::new(Mutex::new(socket));
+        Ok(TokioRawSocket { socket })
+    }
 
-            socket.set_nonblocking(true)?;
-
-            tracing::trace!("Connected to {:?}", &socket_addr);
-            Ok(socket)
+    pub async fn send_to(&self, data: &[u8], addr: socket2::SockAddr) -> std::io::Result<usize> {
+        tracing::trace!(?data, ?addr, "send_to");
+        let socket = self.socket.clone();
+        let cloned_data = data.to_vec();
+        let res = tokio::task::spawn_blocking(move || {
+            let socket = socket.blocking_lock();
+            tracing::trace!(
+                "send_to blocking lock, sending data {:?} to addr {:?}",
+                cloned_data,
+                addr
+            );
+            socket.send_to(cloned_data.as_ref(), &addr)
         })
-        .await?
-        .map_err(|e: std::io::Error| e)?;
+        .await??;
 
-        Ok(TokioRawSocketStream { socket })
+        Ok(res)
+    }
+
+    pub async fn recv_from(&self, buf: &mut [MaybeUninit<u8>]) -> std::io::Result<(usize, SockAddr)> {
+        tracing::trace!("recv_from, buf len: {}", buf.len());
+        let socket = self.socket.clone();
+        let (rx, mut tx) = tokio::sync::mpsc::channel(1);
+        let size = buf.len();
+        let (len, socket_addr) = tokio::task::spawn_blocking(move || {
+            let socket = socket.blocking_lock();
+            let mut inner_buf = vec![MaybeUninit::uninit(); size];
+            let (usize, socket_addr) = socket.recv_from(&mut inner_buf)?;
+            rx.blocking_send(inner_buf)
+                .map_err(|_| std::io::Error::new(std::io::ErrorKind::Other, "Channel failed to send"))?;
+            Ok::<(usize, SockAddr), std::io::Error>((usize, socket_addr))
+        })
+        .await??;
+
+        let inner_buf = tx.recv().await.ok_or(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Channel failed to receive",
+        ))?;
+
+        buf[..len].copy_from_slice(&inner_buf[..len]);
+
+        Ok((len, socket_addr))
+    }
+
+    pub async fn send(&self, data: &[u8]) -> std::io::Result<usize> {
+        let socket = self.socket.clone();
+        let cloned_data = data.to_vec();
+        let res = tokio::task::spawn_blocking(move || {
+            let socket = socket.blocking_lock();
+            socket.send(cloned_data.as_ref())
+        })
+        .await??;
+
+        Ok(res)
+    }
+
+    pub async fn connect(&self, addr: socket2::SockAddr) -> std::io::Result<()> {
+        let socket = self.socket.clone();
+        tokio::task::spawn_blocking(move || {
+            let socket = socket.blocking_lock();
+            socket.connect(&addr)
+        })
+        .await??;
+
+        Ok(())
+    }
+
+    pub async fn bind(&self, addr: socket2::SockAddr) -> std::io::Result<()> {
+        let socket = self.socket.clone();
+        tokio::task::spawn_blocking(move || {
+            let socket = socket.blocking_lock();
+            socket.bind(&addr)
+        })
+        .await??;
+
+        Ok(())
+    }
+
+    pub async fn set_ttl(&self, ttl: u32) -> std::io::Result<()> {
+        let socket = self.socket.clone();
+        tokio::task::spawn_blocking(move || {
+            let socket = socket.blocking_lock();
+            socket.set_ttl(ttl)
+        })
+        .await??;
+
+        Ok(())
+    }
+
+    pub async fn set_read_timeout(&self, timeout: std::time::Duration) -> std::io::Result<()> {
+        self.socket.lock().await.set_read_timeout(Some(timeout))?;
+
+        Ok(())
+    }
+
+    #[inline]
+    pub async fn set_broadcast(&self, broadcast: bool) -> std::io::Result<()> {
+        self.socket.lock().await.set_broadcast(broadcast)
     }
 }
-
-impl AsyncRead for TokioRawSocketStream {
-    fn poll_read(
-        mut self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-        buf: &mut tokio::io::ReadBuf<'_>,
-    ) -> std::task::Poll<std::io::Result<()>> {
-        let mut buffer = [0u8; 8096];
-        match self.socket.read(&mut buffer) {
-            Ok(size) => {
-                tracing::trace!("Read {} bytes, buffer = {:?}", size, &buffer[..size]);
-                buf.put_slice(&buffer[..size]);
-                std::task::Poll::Ready(Ok(()))
-            }
-            Err(e) => {
-                if e.kind() == std::io::ErrorKind::WouldBlock {
-                    cx.waker().wake_by_ref();
-                    std::task::Poll::Pending
-                } else {
-                    std::task::Poll::Ready(Err(e))
-                }
-            }
-        }
-    }
-}
-
-impl AsyncWrite for TokioRawSocketStream {
-    fn poll_write(
-        mut self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-        buf: &[u8],
-    ) -> std::task::Poll<Result<usize, std::io::Error>> {
-        match self.socket.write(buf) {
-            Ok(size) => std::task::Poll::Ready(Ok(size)),
-            Err(e) => {
-                if e.kind() == std::io::ErrorKind::WouldBlock {
-                    cx.waker().wake_by_ref();
-                    std::task::Poll::Pending
-                } else {
-                    std::task::Poll::Ready(Err(e))
-                }
-            }
-        }
-    }
-
-    fn poll_flush(
-        mut self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Result<(), std::io::Error>> {
-        match self.socket.flush() {
-            Ok(_) => std::task::Poll::Ready(Ok(())),
-            Err(e) => {
-                if e.kind() == std::io::ErrorKind::WouldBlock {
-                    cx.waker().wake_by_ref();
-                    std::task::Poll::Pending
-                } else {
-                    std::task::Poll::Ready(Err(e))
-                }
-            }
-        }
-    }
-
-    fn poll_shutdown(
-        self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Result<(), std::io::Error>> {
-        match self.socket.shutdown(std::net::Shutdown::Both) {
-            Ok(_) => std::task::Poll::Ready(Ok(())),
-            Err(e) => {
-                if e.kind() == std::io::ErrorKind::WouldBlock {
-                    cx.waker().wake_by_ref();
-                    std::task::Poll::Pending
-                } else {
-                    std::task::Poll::Ready(Err(e))
-                }
-            }
-        }
-    }
-}
-
-impl Unpin for TokioRawSocketStream {}
