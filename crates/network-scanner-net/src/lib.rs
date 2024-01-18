6d96dfaf5293cf31a429962254d513f9f2f0ab57
irving ou <jou@devolutions.net>
2024-01-17 21:59:37 -0500

    Experimental

--- crates/network-scanner-net/src/lib.rs
+++ crates/network-scanner-net/src/lib.rs
@@ -1,1 +1,12 @@
 pub mod tokio_raw_socket;
+pub mod async_io;
+
+#[derive(Debug,thiserror::Error)]
+pub enum ScannnerNetError{
+
+    #[error("std::io::Error")]
+    StdIoError(std::io::Error),
+    
+    #[error("async run time has failed")]
+    AsyncRuntimeError(anyhow::Error),
+}
