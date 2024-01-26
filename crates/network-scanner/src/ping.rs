2fdf2ee692e91551b73b7887ae56e9f866b67847
irving ou <jou@devolutions.net>
2024-01-26 17:19:36 -0500

    refractor ping based on changes on network-scan-net

--- crates/network-scanner/src/ping.rs
+++ crates/network-scanner/src/ping.rs
@@ -1,13 +1,47 @@
 use std::{
     mem::MaybeUninit,
     net::{Ipv4Addr, SocketAddr},
+    sync::Arc,
 };
 
 use anyhow::Context;
+use network_scanner_net::runtime::Socket2Runtime;
 use network_scanner_proto::icmp_v4;
 
-pub async fn ping(ip: Ipv4Addr) -> anyhow::Result<()> {
-    tokio::task::spawn_blocking(move || blocking_ping(ip)).await?
+pub async fn ping(runtime: Arc<Socket2Runtime>, ip: Ipv4Addr) -> anyhow::Result<()> {
+    let mut socket = runtime.new_socket(
+        socket2::Domain::IPV4,
+        socket2::Type::RAW,
+        Some(socket2::Protocol::ICMPV4),
+    )?;
+
+    let addr = SocketAddr::new(ip.into(), 0);
+
+    let (packet, verifier) = create_echo_request()?;
+    socket
+        .send_to(&packet.to_bytes(true), &addr.into())
+        .await
+        .with_context(|| format!("failed to send packet to {}", ip))?;
+
+    let mut buffer = [MaybeUninit::uninit(); icmp_v4::ICMPV4_MTU];
+    let (size, _) = socket
+        .recv_from(&mut buffer)
+        .await
+        .with_context(|| format!("failed to receive packet from {}", ip))?;
+
+    let filled = unsafe { assume_init(&buffer[..size]) };
+
+    let packet = icmp_v4::Icmpv4Packet::parse(filled).context("cannot parse icmp packet")?;
+
+    if let icmp_v4::Icmpv4Message::EchoReply { payload, .. } = packet.message {
+        if payload != verifier {
+            anyhow::bail!("payload does not match for echo reply");
+        } else {
+            Ok(())
+        }
+    } else {
+        anyhow::bail!("received non-echo reply");
+    }
 }
 
 pub fn blocking_ping(ip: Ipv4Addr) -> anyhow::Result<()> {
