f8ac4dabe13cf84eb95cdd1d50d57b6213860e01
irvingouj @ Devolutions <139169536+irvingoujAtDevolution@users.noreply.github.com>
2024-01-29 16:00:22 -0500

    feat(Network-Scan): Added Network Scan JWT token type (#673)
    * Network-Scan: Token Generation for both Gateway and .Net side
    
    Another half
    
    * ci,fmt
    
    * add test
    
    * small fix
    
    * ci, fmt

--- utils/dotnet/Devolutions.Gateway.Utils.Tests/JsonSerializationTests.cs
+++ utils/dotnet/Devolutions.Gateway.Utils.Tests/JsonSerializationTests.cs
@@ -104,6 +104,16 @@ public class JsonSerialization
     }
 
     [Fact]
+    public void NetworkScanClaims()
+    {
+        const string EXPECTED = """{"jet_gw_id":"ccbaad3f-4627-4666-8bb5-cb6a1a7db815"}""";
+
+        var claims = new NetworkScanClaims(gatewayId);
+        string result = JsonSerializer.Serialize(claims);
+        Assert.Equal(EXPECTED, result);
+    }
+
+    [Fact]
     public void JrlClaims()
     {
         const string EXPECTED = """{"jrl":{"jti":["2dd6fb87-5340-4a85-9e96-d383ebef8a41","01f2b129-bfbf-44fb-8b6e-5cbaf7a71300"]},"jet_gw_id":"ccbaad3f-4627-4666-8bb5-cb6a1a7db815"}""";
---
+++ utils/dotnet/Devolutions.Gateway.Utils/src/NetworkScanClaims.cs
@@ -0,0 +1,21 @@
+using System.Text.Json.Serialization;
+
+namespace Devolutions.Gateway.Utils;
+
+public class NetworkScanClaims : IGatewayClaims
+{
+    [JsonPropertyName("jet_gw_id")]
+    public Guid GatewayId { get; set; }
+
+    public NetworkScanClaims(
+        Guid gatewayId
+    )
+    {
+        this.GatewayId = gatewayId;
+    }
+
+    public string GetContentType()
+    {
+        return "NETWORK_SCAN";
+    }
+}
