b286573e2ceb5c41270281deaea25b06437caf93
irving ou <jou@devolutions.net>
2024-01-29 13:22:29 -0500

    Refractor Ping
    commit 08d5a3920a05b1b45f186254b0964fecd743f6cd
    Author: irving ou <jou@devolutions.net>
    Date:   Sun Jan 28 22:41:06 2024 -0500
    
        Samll fix
    
    commit 2fdf2ee692e91551b73b7887ae56e9f866b67847
    Author: irving ou <jou@devolutions.net>
    Date:   Fri Jan 26 17:19:36 2024 -0500
    
        refractor ping based on changes on network-scan-net
    
    commit 02b55eb4d4cd1e53e9de03bb8455bbd6d3ed62e4
    Author: irving ou <jou@devolutions.net>
    Date:   Fri Jan 26 16:29:58 2024 -0500
    
        give up
    
    commit 32d31b2eeab467209b06bf9084813f54aedd201f
    Author: irving ou <jou@devolutions.net>
    Date:   Fri Jan 26 15:50:58 2024 -0500
    
        fix fix fix ci
    
    commit 1c6a21a01242e6d8b31281722d1ad1996fea127a
    Author: irving ou <jou@devolutions.net>
    Date:   Fri Jan 26 15:01:51 2024 -0500
    
        Update runtime.rs
    
    commit c1c71e13bd9920f1ad4eee80cd2ef81cda01a9c1
    Author: irving ou <jou@devolutions.net>
    Date:   Fri Jan 26 14:59:54 2024 -0500
    
        add none event type
    
    commit 9a73f5e1a44905a3d4e38729396e443a7e1affcf
    Author: irving ou <jou@devolutions.net>
    Date:   Fri Jan 26 14:34:30 2024 -0500
    
        Correctly drop runtime
    
        Update drop_runtime.rs
    
    commit 572e56b451d08c14503bc832161cc256e2802c0c
    Author: irving ou <jou@devolutions.net>
    Date:   Fri Jan 26 14:14:59 2024 -0500
    
        fmt
    
    commit 4d043f78ef78f86eb63487c8635e82522d1f714b
    Author: irving ou <jou@devolutions.net>
    Date:   Fri Jan 26 14:12:56 2024 -0500
    
        remove tracing_subscriber
    
    commit 002a95daedb37166ab7a051e407c73adeff8f9ba
    Author: irving ou <jou@devolutions.net>
    Date:   Fri Jan 26 13:46:48 2024 -0500
    
        update
    
    commit 83c4b70bd73378c497fc7703752a80604c14b7e9
    Author: irving ou <jou@devolutions.net>
    Date:   Fri Jan 26 13:45:25 2024 -0500
    
        Try fix CI again, with tracing trace this time
    
    commit 4948b6ad66a6ff67396fe68ad1fcbcf0ab624f7d
    Author: irving ou <jou@devolutions.net>
    Date:   Thu Jan 25 15:20:13 2024 -0500
    
        add timeout for test
    
    commit 6c1544d357ebc0219986e794bb3ac26de303f351
    Author: irving ou <jou@devolutions.net>
    Date:   Thu Jan 25 13:11:19 2024 -0500
    
        refractor with fix in test
    
    commit 602d0da497f7cbd54ddb5aeb6f6edda84b7c5b18
    Author: irving ou <jou@devolutions.net>
    Date:   Wed Jan 24 12:16:43 2024 -0500
    
        try fix ci 2
    
    commit 42d704276b18e159aa3dbc06b55899dab779c105
    Author: irvingouj @ Devolutions <139169536+irvingoujAtDevolution@users.noreply.github.com>
    Date:   Wed Jan 24 11:29:39 2024 -0500
    
        remove tracing
    
    commit 89ec87128f2162f03c2f775430497fde7c21175b
    Author: irving ou <jou@devolutions.net>
    Date:   Wed Jan 24 10:46:54 2024 -0500
    
        ci
    
    commit ed518c589a6f0d3de7987fbf381e405cfd2dfa72
    Author: irving ou <jou@devolutions.net>
    Date:   Wed Jan 24 10:43:59 2024 -0500
    
        test CI again

--- crates/network-scanner/examples/ping.rs
+++ crates/network-scanner/examples/ping.rs
@@ -1,11 +1,13 @@
 use network_scanner::ping::ping;
 
 #[tokio::main]
-pub async fn main() {
+pub async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::SubscriberBuilder::default()
         .with_max_level(tracing::Level::TRACE)
         .init();
 
-    let ip = std::net::Ipv4Addr::new(127, 0, 0, 1);
-    ping(ip).await.expect("Failed to ping");
+    let ip = std::net::Ipv4Addr::new(8, 8, 8, 8); //famous google dns
+    let runtime = network_scanner_net::runtime::Socket2Runtime::new(None)?;
+    ping(runtime, ip).await.expect("Failed to ping");
+    Ok(())
 }
