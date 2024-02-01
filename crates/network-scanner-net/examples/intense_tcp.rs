9e956f2e2c7c264898ff8c0fb5fece39416ef594
irvingouj @ Devolutions <139169536+irvingoujAtDevolution@users.noreply.github.com>
2024-02-01 09:03:55 -0500

    refactor(dgw): improve network-scanner-net crate (#665)

---
+++ crates/network-scanner-net/examples/intense_tcp.rs
@@ -0,0 +1,137 @@
+use std::{
+    mem::MaybeUninit,
+    net::{SocketAddr, SocketAddrV4},
+    sync::atomic::AtomicU32,
+    time::Instant,
+};
+
+use network_scanner_net::socket::AsyncRawSocket;
+use socket2::SockAddr;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    task::JoinHandle,
+};
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 12)]
+pub async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::SubscriberBuilder::default()
+        .with_max_level(tracing::Level::DEBUG)
+        .with_thread_names(true)
+        .init();
+    // trace info all args
+    let args: Vec<String> = std::env::args().collect();
+    tracing::info!("Args: {:?}", std::env::args().collect::<Vec<String>>());
+    if args.len() < 4 {
+        println!("Usage: {} [server|client] -p <port>", args[0]);
+        return Ok(());
+    }
+
+    let port = args[args.len() - 1].parse::<u16>()?;
+    let addr = format!("127.0.0.1:{}", port);
+    match args[1].as_str() {
+        "server" => {
+            tcp_server(&addr).await?;
+        }
+        "client" => {
+            tcp_client().await?;
+        }
+        _ => {
+            println!("Usage: {} [server|client] -p <port>", args[0]);
+        }
+    }
+
+    Ok(())
+}
+
+async fn tcp_client() -> anyhow::Result<()> {
+    let async_runtime = network_scanner_net::runtime::Socket2Runtime::new(None)?;
+    let mut socket_arr = vec![];
+    for _ in 0..100 {
+        tracing::info!("Creating socket");
+        let socket = async_runtime.new_socket(socket2::Domain::IPV4, socket2::Type::STREAM, None)?;
+        socket_arr.push(socket);
+    }
+
+    fn connect_and_write_something(
+        mut socket: AsyncRawSocket,
+        addr: std::net::SocketAddr,
+    ) -> JoinHandle<anyhow::Result<()>> {
+        tokio::task::spawn(async move {
+            let addr: SockAddr = addr.into();
+            socket.connect(&addr).await?;
+            let mut buffer = [MaybeUninit::uninit(); 1024];
+            for i in 0..1000 {
+                let data = format!("hello world {} times", i);
+                tracing::info!("Sending: {} from socket {:?}", &data, &socket);
+                let write_future = socket.send(data.as_bytes());
+
+                let size = tokio::time::timeout(std::time::Duration::from_secs(1), write_future).await??;
+
+                if size == 0 {
+                    return Ok(());
+                }
+
+                let recv_future = socket.recv(&mut buffer);
+                tokio::time::timeout(std::time::Duration::from_secs(1), recv_future).await??;
+                let received = buffer[..size]
+                    .iter()
+                    .map(|x| unsafe { x.assume_init() })
+                    .collect::<Vec<u8>>();
+                assert_eq!(received, data.as_bytes());
+                tracing::debug!("Received: {}", std::str::from_utf8(&received)?);
+            }
+            Ok(())
+        })
+    }
+
+    let mut handles = vec![];
+    for socket in socket_arr {
+        let addr: SocketAddr = SocketAddrV4::new(std::net::Ipv4Addr::new(127, 0, 0, 1), 8080).into();
+
+        let handle = connect_and_write_something(socket, addr);
+        handles.push(handle);
+    }
+
+    for future in handles {
+        future.await??;
+    }
+
+    Ok(())
+}
+
+async fn tcp_server(addr: &str) -> anyhow::Result<()> {
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+    println!("Listening on: {}", addr);
+    let count = std::sync::Arc::new(AtomicU32::new(0));
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        let _now = Instant::now();
+        let count = count.clone();
+        tokio::spawn(async move {
+            let mut buf = vec![0; 1024];
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    // socket closed
+                    Ok(n) => {
+                        if n == 0 {
+                            println!("Socket closed");
+                            return;
+                        }
+                        n
+                    }
+                    Err(e) => {
+                        eprintln!("Failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+                println!("Received {} bytes", n);
+                // Write the data back
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("Failed to write to socket; err = {:?}", e);
+                    return;
+                }
+                count.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+            }
+        });
+    }
+}
--- crates/network-scanner-net/src/runtime.rs
+++ crates/network-scanner-net/src/runtime.rs
@@ -1,5 +1,6 @@
 use std::{
-    collections::HashMap,
+    collections::{HashMap, HashSet},
+    hash::Hash,
     num::NonZeroUsize,
     sync::{
         atomic::{AtomicBool, AtomicUsize, Ordering},
@@ -10,6 +11,7 @@ use std::{
 
 use anyhow::Context;
 use crossbeam::channel::{Receiver, Sender};
+use parking_lot::Mutex;
 use polling::{Event, Events};
 use socket2::Socket;
 
@@ -17,16 +19,18 @@ use crate::{socket::AsyncRawSocket, ScannnerNetError};
 
 #[derive(Debug)]
 pub struct Socket2Runtime {
-    poller: polling::Poller,
+    poller: Arc<polling::Poller>,
     next_socket_id: AtomicUsize,
-    is_terminated: AtomicBool,
-    sender: Sender<RegisterEvent>,
+    is_terminated: Arc<AtomicBool>,
+    register_sender: Sender<RegisterEvent>,
+    event_receiver: Receiver<Event>,
+    event_cache: Mutex<HashSet<EventWrapper>>,
 }
 
 impl Drop for Socket2Runtime {
     fn drop(&mut self) {
-        self.is_terminated.store(true, Ordering::SeqCst);
         tracing::debug!("dropping runtime");
+        self.is_terminated.store(true, Ordering::SeqCst);
         let _ = self // ignore errors, cannot handle it here
             .poller
             .notify()
@@ -41,15 +45,22 @@ impl Socket2Runtime {
     /// Create a new runtime with a queue capacity, default is 1024.
     pub fn new(queue_capacity: Option<usize>) -> anyhow::Result<Arc<Self>> {
         let poller = polling::Poller::new()?;
-        let (sender, receiver) = crossbeam::channel::bounded(queue_capacity.unwrap_or(QUEUE_CAPACITY));
+
+        let (register_sender, register_receiver) =
+            crossbeam::channel::bounded(queue_capacity.unwrap_or(QUEUE_CAPACITY));
+
+        let (event_sender, event_receiver) = crossbeam::channel::bounded(queue_capacity.unwrap_or(QUEUE_CAPACITY));
+
         let runtime = Self {
-            poller,
+            poller: Arc::new(poller),
             next_socket_id: AtomicUsize::new(0),
-            is_terminated: AtomicBool::new(false),
-            sender,
+            is_terminated: Arc::new(AtomicBool::new(false)),
+            register_sender,
+            event_receiver,
+            event_cache: Mutex::new(HashSet::new()),
         };
         let runtime = Arc::new(runtime);
-        runtime.clone().start_loop(receiver)?;
+        runtime.start_loop(register_receiver, event_sender)?;
         Ok(runtime)
     }
 
@@ -67,37 +78,51 @@ impl Socket2Runtime {
         Ok(AsyncRawSocket::from_socket(socket, id, self.clone())?)
     }
 
-    pub(crate) fn remove_socket(&self, socket: &socket2::Socket) -> anyhow::Result<()> {
+    pub(crate) fn remove_socket(&self, socket: &socket2::Socket, id: usize) -> anyhow::Result<()> {
         self.poller.delete(socket)?;
+        // remove all events related to this socket
+        self.event_cache.lock().retain(|event| id == event.0.key);
         Ok(())
     }
 
-    fn start_loop(self: Arc<Self>, receiver: Receiver<RegisterEvent>) -> anyhow::Result<()> {
+    fn start_loop(
+        &self,
+        register_receiver: Receiver<RegisterEvent>,
+        event_sender: Sender<Event>,
+    ) -> anyhow::Result<()> {
+        // we make is_terminated Arc<AtomicBool> and poller Arc<Poller> so that we can clone them and move them into the thread
+        // we cannot hold a Arc<Socket2Runtime> in the thread, because it will create a cycle reference and the runtime will never be dropped.
+        let is_terminated = self.is_terminated.clone();
+        let poller = self.poller.clone();
         std::thread::Builder::new()
             .name("[raw-socket]:io-event-loop".to_string())
             .spawn(move || {
                 let mut events = Events::with_capacity(NonZeroUsize::new(1024).unwrap());
                 tracing::debug!("starting io event loop");
+                // events registered but not happened yet
                 let mut events_registered = HashMap::new();
-                let mut events_happend = HashMap::new();
+
+                // events happened but not registered yet
+                let mut events_happened = HashMap::new();
+
                 loop {
-                    if self.is_terminated.load(Ordering::Acquire) {
+                    if is_terminated.load(Ordering::Acquire) {
                         break;
                     }
 
                     tracing::debug!("polling events");
-                    if let Err(e) = self.poller.wait(&mut events, None) {
+                    if let Err(e) = poller.wait(&mut events, None) {
                         tracing::error!(error = ?e, "failed to poll events");
-                        self.is_terminated.store(true, Ordering::SeqCst);
+                        is_terminated.store(true, Ordering::SeqCst);
                         break;
                     };
                     for event in events.iter() {
-                        tracing::trace!(?event, "event happend");
-                        events_happend.insert(event.key, event);
+                        tracing::trace!(?event, "event happened");
+                        events_happened.insert(event.key, event);
                     }
                     events.clear();
 
-                    while let Ok(event) = receiver.try_recv() {
+                    while let Ok(event) = register_receiver.try_recv() {
                         match event {
                             RegisterEvent::Register { id, waker } => {
                                 events_registered.insert(id, waker);
@@ -108,23 +133,77 @@ impl Socket2Runtime {
                         }
                     }
 
-                    let intersection = events_happend
+                    let intersection = events_happened
                         .keys()
                         .filter(|key| events_registered.contains_key(key))
                         .cloned()
                         .collect::<Vec<_>>();
 
                     intersection.into_iter().for_each(|ref key| {
-                        let event = events_happend.remove(key).unwrap();
+                        let event = events_happened.remove(key).unwrap();
                         let waker = events_registered.remove(key).unwrap();
+                        let _ = event_sender.try_send(event);
                         waker.wake_by_ref();
                         tracing::trace!(?event, "waking up waker");
                     });
                 }
+                tracing::info!("io event loop terminated");
             })
             .with_context(|| "failed to spawn io event loop thread")?;
 
         Ok(())
+    }
+
+    /// Ideally, we should have a dedicated thread to handle events we received, but we don't really want to spawn a second thread
+    /// Alternatively, we can have all socket futures call this function to check if there is any event for them.
+    /// The number of times the socket futures is polled is almost guaranteed to be more than the number of registration we received.
+    /// hence the event receiver will not be blocked.
+    pub(crate) fn check_event(&self, event: Event, remove: bool) -> Option<Event> {
+        let mut event_cache = self.event_cache.lock();
+        while let Ok(event) = self.event_receiver.try_recv() {
+            event_cache.insert(event.into());
+        }
+        tracing::debug!("checking event cache {:?}", event_cache);
+
+        let event = if remove {
+            event_cache.take(&event.into())
+        } else if event_cache.contains(&event.into()) {
+            Some(event.into())
+        } else {
+            None
+        };
+
+        event.map(|event| event.into_inner())
+    }
+
+    pub(crate) fn check_event_with_id(&self, id: usize, remove: bool) -> Vec<Event> {
+        let mut event_cache = self.event_cache.lock();
+        while let Ok(event) = self.event_receiver.try_recv() {
+            event_cache.insert(event.into());
+        }
+        let event_interested = vec![
+            Event::readable(id),
+            Event::writable(id),
+            Event::all(id),
+            Event::none(id),
+        ];
+        let mut res = vec![];
+
+        if remove {
+            event_interested.into_iter().for_each(|event| {
+                if let Some(event) = event_cache.take(&event.into()) {
+                    res.push(event.into_inner());
+                }
+            });
+        } else {
+            event_interested.into_iter().for_each(|event| {
+                if event_cache.contains(&event.into()) {
+                    res.push(event);
+                }
+            });
+        }
+
+        res
     }
 
     pub(crate) fn register(&self, socket: &Socket, event: Event, waker: Waker) -> anyhow::Result<()> {
@@ -138,7 +217,7 @@ impl Socket2Runtime {
         // Use try_send instead of send, in case some io events blocked the queue completely,
         // it would be better to drop the register event then block the worker thread or main thread.
         // as the worker thread is shared for the entire application.
-        self.sender
+        self.register_sender
             .try_send(RegisterEvent::Register { id: event.key, waker })
             .with_context(|| "failed to send register event to register loop")
     }
@@ -147,7 +226,7 @@ impl Socket2Runtime {
         if self.is_terminated.load(Ordering::Acquire) {
             Err(ScannnerNetError::AsyncRuntimeError("runtime is terminated".to_string()))?;
         }
-        self.sender
+        self.register_sender
             .try_send(RegisterEvent::Unregister { id })
             .with_context(|| "failed to send unregister event to register loop")
     }
@@ -157,4 +236,35 @@ impl Socket2Runtime {
 enum RegisterEvent {
     Register { id: usize, waker: Waker },
     Unregister { id: usize },
+}
+
+#[derive(Debug)]
+pub struct EventWrapper(Event);
+
+impl Hash for EventWrapper {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.0.key.hash(state);
+        self.0.readable.hash(state);
+        self.0.writable.hash(state);
+    }
+}
+
+impl PartialEq for EventWrapper {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.key == other.0.key && self.0.readable == other.0.readable && self.0.writable == other.0.writable
+    }
+}
+
+impl Eq for EventWrapper {}
+
+impl From<Event> for EventWrapper {
+    fn from(event: Event) -> Self {
+        Self(event)
+    }
+}
+
+impl EventWrapper {
+    pub(crate) fn into_inner(self) -> Event {
+        self.0
+    }
 }
