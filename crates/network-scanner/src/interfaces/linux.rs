c9b331a7e34d7d38ee37354dee8e086a709fb31b
Irving Ou <jou@devolutions.net>
2024-02-15 11:04:50 -0500

    ci

--- crates/network-scanner/src/interfaces/linux.rs
+++ crates/network-scanner/src/interfaces/linux.rs
@@ -1,15 +1,15 @@
-use std::{net::IpAddr, num::NonZeroI32};
 use anyhow::Context;
+use std::{net::IpAddr, num::NonZeroI32};
 use tokio::sync::mpsc::Receiver;
 
 use crate::interfaces::NetworkInterface;
+use futures_util::stream::TryStreamExt;
 use netlink_packet_route::{
     address::{AddressAttribute, AddressMessage},
     link::{LinkAttribute, LinkFlag},
     route::{RouteAddress, RouteAttribute, RouteMessage},
 };
 use rtnetlink::{new_connection, Handle};
-use futures_util::stream::TryStreamExt;
 
 pub fn get_network_interfaces() -> anyhow::Result<Vec<NetworkInterface>> {
     let (connection, handle, _) = new_connection()?;
