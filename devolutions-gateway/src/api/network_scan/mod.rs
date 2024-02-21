440d8c2ac9e48e7943d107a8ec2a22951c6efa57
Benoît Cortier <bcortier@proton.me>
2024-02-21 00:45:43 -0500

    Update devolutions-gateway/src/api/network_scan/mod.rs

--- devolutions-gateway/src/api/network_scan/mod.rs
+++ devolutions-gateway/src/api/network_scan/mod.rs
@@ -7,6 +7,6 @@ pub mod scan;
 pub fn make_router<S>(state: DgwState) -> Router<S> {
     Router::new()
         .route("/scan", axum::routing::get(scan::handler))
-        .route("/ipconfig", axum::routing::get(ipconfig::handler))
+        .route("/config", axum::routing::get(ipconfig::handler))
         .with_state(state)
 }
