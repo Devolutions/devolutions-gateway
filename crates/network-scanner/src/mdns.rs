939901bcd7f3396e57ac23dfba135e92b9a05a23
Benoît Cortier <bcortier@proton.me>
2024-02-27 04:03:54 -0500

    Update crates/network-scanner/src/mdns.rs

--- crates/network-scanner/src/mdns.rs
+++ crates/network-scanner/src/mdns.rs
@@ -158,7 +158,7 @@ impl TryFrom<&str> for ServiceType {
             "_telnet._tcp" => Ok(ServiceType::Telnet),
             "_ldap._tcp" => Ok(ServiceType::Ldap),
             "_ldaps._tcp" => Ok(ServiceType::Ldaps),
-            // Ard is a special type of Vnc
+            // ARD is a variant of RFB (VNC) protocol.
             "_rfb._tcp" => Ok(ServiceType::Vnc),
             "_rdp._tcp" | "_rdp._udp" => Ok(ServiceType::Rdp),
             _ => Err(anyhow::anyhow!("unknown protocol: {}", value)),
