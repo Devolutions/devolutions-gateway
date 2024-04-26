2032ae19b2a7febf1e0a5ca9697b36d1d1ed45bc
Benoît CORTIER <bcortier@proton.me>
2024-04-26 12:16:04 +0900

    feat(dgw): return disk space available for recordings
    The total and available space used for storing recordings is now
    returned in the heartbeat response.
    
    If the system does not support this operation, the fields are
    excluded from the response.
    
    Issue: DGW-100

--- devolutions-gateway/src/api/heartbeat.rs
+++ devolutions-gateway/src/api/heartbeat.rs
@@ -17,6 +17,12 @@ pub(crate) struct Heartbeat {
     version: &'static str,
     /// Number of running sessions
     running_session_count: usize,
+    /// The total space of the disk used to store recordings, in bytes.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    recording_storage_total_space: Option<u64>,
+    /// The remaining available space to store recordings, in bytes.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    recording_storage_available_space: Option<u64>,
 }
 
 /// Performs a heartbeat check
@@ -39,6 +45,8 @@ pub(super) async fn get_heartbeat(
     }): State<DgwState>,
     _scope: HeartbeatReadScope,
 ) -> Result<Json<Heartbeat>, HttpError> {
+    use sysinfo::Disks;
+
     let conf = conf_handle.get_conf();
 
     let running_session_count = sessions
@@ -46,10 +54,51 @@ pub(super) async fn get_heartbeat(
         .await
         .map_err(HttpError::internal().err())?;
 
+    let (recording_storage_total_space, recording_storage_available_space) = if sysinfo::IS_SUPPORTED_SYSTEM {
+        trace!("System is supporting listing storage disks");
+
+        let recording_path = conf
+            .recording_path
+            .canonicalize()
+            .unwrap_or_else(|_| conf.recording_path.clone().into_std_path_buf());
+
+        let disks = Disks::new_with_refreshed_list();
+
+        debug!(?disks, "Found disks");
+
+        let mut recording_disk = None;
+        let mut longest_path = 0;
+
+        for disk in disks.list() {
+            let mount_point = disk.mount_point();
+            let path_len = mount_point.components().count();
+            if recording_path.starts_with(mount_point) && longest_path < path_len {
+                recording_disk = Some(disk);
+                longest_path = path_len;
+            }
+        }
+
+        if let Some(disk) = recording_disk {
+            debug!(?disk, "Disk used to store recordings");
+
+            (Some(disk.total_space()), Some(disk.available_space()))
+        } else {
+            warn!("Failed to find disk used for recording storage");
+
+            (None, None)
+        }
+    } else {
+        debug!("This system does not support listing storage disks");
+
+        (None, None)
+    };
+
     Ok(Json(Heartbeat {
         id: conf.id,
         hostname: conf.hostname.clone(),
         version: env!("CARGO_PKG_VERSION"),
         running_session_count,
+        recording_storage_total_space,
+        recording_storage_available_space,
     }))
 }
