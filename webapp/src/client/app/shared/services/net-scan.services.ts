f44738129446f3d609956d09674440b8efa7155e
irvingouj @ Devolutions <139169536+irvingoujAtDevolution@users.noreply.github.com>
2024-05-07 15:36:53 -0400

    fix(webapp): fix netscan result duplicate and performance improvement (#845)
    * only display non-duplicated entry
    
    * only display non-duplicated entry
    
    * only display non-duplicated entry
    
    * one scan per websession

--- webapp/src/client/app/shared/services/net-scan.services.ts
+++ webapp/src/client/app/shared/services/net-scan.services.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, Subject, from, of } from 'rxjs';
+import { Observable, Subject, from, merge, of } from 'rxjs';
 import { Protocol } from '../enums/web-client-protocol.enum';
 import {
   ProtocolIconMap,
@@ -24,51 +24,54 @@ export class NetScanService {
   private serviceUpdatePipe: Subject<NetScanEntry> =
     new Subject<NetScanEntry>();
 
-  constructor(private webClientService: WebClientService) {}
+  // JS set doesn't allow customized equality check, so we stringify for deep comparison
+  private serviceCache: Set<String> = new Set<String>();
+  private scanSubject = new Subject<NetScanEntry>();
 
-  public startScan(): Observable<NetScanEntry> {
-    return new Observable<NetScanEntry>((observer) => {
-      this.webClientService
-        .fetchNetScanToken()
-        .pipe(
-          map((token: string) => {
-            let path = `${this.scanUrl}?token=${token}`;
-            let url_http = new URL(path, window.location.href).toString();
-            let url = url_http.replace('http', 'ws');
-            return new WebSocket(url);
-          })
-        )
-        .subscribe((socket: WebSocket) => {
+  constructor(private webClientService: WebClientService) {
+    this.webClientService
+      .fetchNetScanToken()
+      .pipe(
+        map((token: string) => {
+          let path = `${this.scanUrl}?token=${token}`;
+          let url_http = new URL(path, window.location.href).toString();
+          let url = url_http.replace('http', 'ws');
+          return new WebSocket(url);
+        })
+      )
+      .subscribe({
+        next: (socket: WebSocket) => {
           socket.onmessage = (event) => {
-            let entry: {
-              ip: string;
-              hostname: string;
-              protocol: string;
-            } = JSON.parse(event.data);
-
-            let protocol = ProtocolNameToProtocolMap[entry.protocol];
-            // We don't yet support this protocol
-            if (!protocol) {
-              return;
-            }
-            let value = {
-              ip: entry.ip,
-              hostname: entry.hostname,
-              protocol: protocol,
-              icon: () => {
-                return ProtocolIconMap[protocol];
-              }
-            };
-            observer.next(value);
+            this.socketOnMessage(event);
           };
-
           socket.onclose = () => {
-            observer.complete();
+            this.scanSubject.complete();
           };
-        });
-    });
+          socket.onerror = () => {
+            this.scanSubject.error('Error scanning network');
+          };
+        },
+
+        error: (error) => {
+          // Error create the websocket
+          this.scanSubject.error(error);
+        },
+      });
   }
 
+  public startScan(): Observable<NetScanEntry> {
+    const existingObservable = from(this.serviceCache).pipe(
+      map((entry: string) => {
+        let toAdd = JSON.parse(entry);
+        toAdd.icon = () => {
+          return ProtocolIconMap[toAdd.protocol];
+        };
+        return toAdd;
+      })
+    );
+
+    return merge(existingObservable, this.scanSubject.asObservable());
+  }
 
   serviceSelected(entry: NetScanEntry) {
     this.serviceUpdatePipe.next(entry);
@@ -76,5 +79,36 @@ export class NetScanService {
 
   onServiceSelected(): Observable<NetScanEntry> {
     return this.serviceUpdatePipe.asObservable();
+  }
+
+  socketOnMessage(event) {
+    let entry: {
+      ip: string;
+      hostname: string;
+      protocol: string;
+    } = JSON.parse(event.data);
+
+    let protocol = ProtocolNameToProtocolMap[entry.protocol];
+    // We don't yet support this protocol
+    if (!protocol) {
+      return;
+    }
+
+    let value = {
+      ip: entry.ip,
+      hostname: entry.hostname,
+      protocol: protocol,
+    };
+
+    if (this.serviceCache.has(JSON.stringify(value))) {
+      return;
+    }
+    this.serviceCache.add(JSON.stringify(value));
+    this.scanSubject.next({
+      ...value,
+      icon: () => {
+        return ProtocolIconMap[protocol];
+      },
+    });
   }
 }
