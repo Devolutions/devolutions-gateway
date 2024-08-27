fc5c39e9d7d0ff95c073723ebb638ac16ea4008d
Vladyslav Nikonov <mail@pacmancoder.xyz>
2024-08-27 17:28:03 +0300

    Updated tlk to build devolutions-host

--- ci/tlk.ps1
+++ ci/tlk.ps1
@@ -122,7 +122,7 @@ function Get-DestinationSymbolFile {
 
     $DestinationSymbolsName = $(Split-Path $DestinationExecutable -LeafBase) + ".$($Target.SymbolsExtension)"
     $DestinationDirectory  = Split-Path $DestinationExecutable -Parent
-    
+
     Join-Path $DestinationDirectory $DestinationSymbolsName
 }
 
@@ -345,6 +345,7 @@ class TlkRecipe
                 if ($this.Target.IsWindows()) {
                     $agentPackages += [TlkPackage]::new("devolutions-pedm-hook", "crates/devolutions-pedm-hook", $true)
                     $agentPackages += [TlkPackage]::new("devolutions-pedm-shell-ext", "crates/devolutions-pedm-shell-ext", $true)
+                    $agentPackages += [TlkPackage]::new("devolutions-host", "crates/devolutions-host", $false)
                 }
 
                 $agentPackages
@@ -405,13 +406,13 @@ class TlkRecipe
         $CargoPackages = $this.CargoPackages()
         $CargoTarget = $this.Target.CargoTarget()
         $CargoProfile = $this.Target.CargoProfile
-        
+
         $CargoOutputPath = "$($this.SourcePath)/target/${CargoTarget}/${CargoProfile}"
 
         foreach ($CargoPackage in $CargoPackages) {
             Push-Location
             Set-Location -Path $CargoPackage.Path
-    
+
             $this.Cargo(@('build'))
 
             $SrcBinaryPath = "${CargoOutputPath}/$($CargoPackage.BinaryFileName($this.Target))"
@@ -455,10 +456,10 @@ class TlkRecipe
                 if (Test-Path Env:STRIP_EXECUTABLE) {
                     $StripExecutable = $Env:STRIP_EXECUTABLE
                 }
-    
+
                 & $StripExecutable $SrcBinaryPath | Out-Host
             }
-    
+
             if ($DestinationExecutable) {
                 Copy-Item -Path $SrcBinaryPath -Destination $DestinationExecutable
             }
@@ -472,7 +473,7 @@ class TlkRecipe
 
                 Push-Location
                 Set-Location $CargoOutputPath
-    
+
                 $MakeAppxOutput = & 'MakeAppx.exe' 'pack' '/f' "${CargoPackagePath}/mapping.txt" '/p' "./DevolutionsPedmShellExt.msix" '/nv' '/o'
                 if (!$?) {
                     throw "MakeAppx package creation failed: ${MakeAppxOutput}"
@@ -497,7 +498,7 @@ class TlkRecipe
 
                 Copy-Item -Path $builtDesktopExe -Destination $Env:DAGENT_PEDM_DESKTOP_EXECUTABLE
                 Copy-Item -Path $builtDesktopPdb -Destination $(Get-DestinationSymbolFile $Env:DAGENT_PEDM_DESKTOP_EXECUTABLE $this.Target)
-                
+
             }
         }
 
