096f8f8509e5fdd80bf52885ae74190fb3e4463e
Benoît CORTIER <bcortier@proton.me>
2024-08-28 20:06:06 +0900

    .

--- crates/devolutions-pedm/src/api/mod.rs
+++ crates/devolutions-pedm/src/api/mod.rs
@@ -61,9 +61,9 @@ struct RawNamedPipeConnectInfo {
 
 impl Connected<&NamedPipeServer> for RawNamedPipeConnectInfo {
     fn connect_info(target: &NamedPipeServer) -> Self {
-        Self {
-            handle: Handle::new_borrowed(HANDLE(target.as_raw_handle().cast())),
-        }
+        let handle = HANDLE(target.as_raw_handle().cast());
+        let handle = Handle::new_borrowed(handle).expect("the handled held by NamedPipeServer is valid");
+        Self { handle }
     }
 }
 
