3e7ca673c5bdf61645f58dd418922c7978a0ac5e
Benoît CORTIER <bcortier@proton.me>
2024-09-01 00:09:23 +0900

    chore(dotnet-client): bump RestSharp 112.0.0
    This version is fixing a vulnerability.

--- devolutions-gateway/openapi/dotnet-client/src/Devolutions.Gateway.Client/Client/Configuration.cs
+++ devolutions-gateway/openapi/dotnet-client/src/Devolutions.Gateway.Client/Client/Configuration.cs
@@ -34,7 +34,7 @@ namespace Devolutions.Gateway.Client.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "2024.8.30";
+        public const string Version = "2024.9.1";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -118,7 +118,7 @@ namespace Devolutions.Gateway.Client.Client
         public Configuration()
         {
             Proxy = null;
-            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/2024.8.30/csharp");
+            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/2024.9.1/csharp");
             BasePath = "http://localhost";
             DefaultHeaders = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
@@ -541,7 +541,7 @@ namespace Devolutions.Gateway.Client.Client
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
             report += "    Version of the API: 2024.3.2\n";
-            report += "    SDK Package Version: 2024.8.30\n";
+            report += "    SDK Package Version: 2024.9.1\n";
 
             return report;
         }
