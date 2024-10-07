bdcb4d50398bc81ed0b1edd23886dedaa60ad3ec
irving ou <jou@devolutions.net>
2024-10-07 14:27:34 -0400

    chore(webapp) enforce biome in recording players

--- webapp/player/js/trp-decoder.js
+++ webapp/player/js/trp-decoder.js
@@ -1,56 +1,55 @@
-
 export function convertTRPtoCast(fileArray) {
-  var castHeader = {
+  const castHeader = {
     version: 2,
     width: 0,
     height: 0,
   };
-  var castEvents = [];
+  const castEvents = [];
 
-  var time = 0.0;
-  var position = 0;
+  let time = 0.0;
+  let position = 0;
   while (position < fileArray.length) {
-    var timer = readUInt32(fileArray, position);
-    var type = readUInt16(fileArray, position + 4);
-    var size = readUInt16(fileArray, position + 6);
-    var chunk = fileArray.subarray(position + 8, position + 8 + size);
+    const timer = readUInt32(fileArray, position);
+    const type = readUInt16(fileArray, position + 4);
+    const size = readUInt16(fileArray, position + 6);
+    const chunk = fileArray.subarray(position + 8, position + 8 + size);
     position += 8 + size;
     time += timer / 1000;
-    if (castEvents.length>0 && castEvents[castEvents.length-1][0] === time) {
+    if (castEvents.length > 0 && castEvents[castEvents.length - 1][0] === time) {
       time += 0.001;
     }
-    if (type == 0) {
+    if (type === 0) {
       // RECORD_CHUNK_TERMINAL_OUTPUT
-      var data = new TextDecoder().decode(chunk);
-      castEvents.push([time, "o", data]);
-    } else if (type == 1) {
+      const data = new TextDecoder().decode(chunk);
+      castEvents.push([time, 'o', data]);
+    } else if (type === 1) {
       // RECORD_CHUNK_USER_INPUT
-      var data = new TextDecoder().decode(chunk);
-      castEvents.push([time, "i", data]);
-    } else if (type == 2) {
+      const data = new TextDecoder().decode(chunk);
+      castEvents.push([time, 'i', data]);
+    } else if (type === 2) {
       // RECORD_CHUNK_SIZE_CHANGE
-      var width = readUInt16(chunk, 0);
-      var height = readUInt16(chunk, 2);
-      if (castHeader.width == 0) {
+      const width = readUInt16(chunk, 0);
+      const height = readUInt16(chunk, 2);
+      if (castHeader.width === 0) {
         castHeader.width = width;
         castHeader.height = height;
       } else {
-        castEvents.push([time, "r", width + "x" + height]);
+        castEvents.push([time, 'r', width + 'x' + height]);
       }
-    } else if (type == 4) {
+    } else if (type === 4) {
       // RECORD_CHUNK_TERMINAL_SETUP
-      var tagCount = size / 6;
-      for (var i = 0; i < tagCount; i++) {
-        var tag = readUInt16(chunk, i * 6);
-        var tagValue = readUInt32(chunk, i * 6 + 2);
+      const tagCount = size / 6;
+      for (let i = 0; i < tagCount; i++) {
+        const _tag = readUInt16(chunk, i * 6);
+        const _tagValue = readUInt32(chunk, i * 6 + 2);
       }
     }
   }
   castHeader.duration = time;
-  var castFile = JSON.stringify(castHeader) + "\n";
-  castEvents.forEach((event) => {
-    castFile += JSON.stringify(event) + "\n";
-  });
+  let castFile = JSON.stringify(castHeader) + '\n';
+  for (const event of castEvents) {
+    castFile += JSON.stringify(event) + '\n';
+  };
   return castFile;
 }
 
