c41e48afac5806f4007d698b7d59106253548af3
Allan <6740989+allan2@users.noreply.github.com>
2024-10-23 15:07:37 -0400

    refactor(jetsocat): fix clippy single_match

--- jetsocat/src/doctor.rs
+++ jetsocat/src/doctor.rs
@@ -437,15 +437,16 @@ mod rustls_checks {
             now,
             ring_crypto_provider.signature_verification_algorithms.all,
         )
-        .inspect_err(|error| match error {
-            Error::InvalidCertificate(cert_error) => match cert_error {
-                rustls::CertificateError::Expired => help::cert_is_expired(ctx),
-                rustls::CertificateError::NotValidYet => help::cert_is_not_yet_valid(ctx),
-                rustls::CertificateError::UnknownIssuer => help::cert_unknown_issuer(ctx),
-                rustls::CertificateError::InvalidPurpose => help::cert_invalid_purpose(ctx),
-                _ => (),
-            },
-            _ => (),
+        .inspect_err(|error| {
+            if let Error::InvalidCertificate(cert_error) = error {
+                match cert_error {
+                    rustls::CertificateError::Expired => help::cert_is_expired(ctx),
+                    rustls::CertificateError::NotValidYet => help::cert_is_not_yet_valid(ctx),
+                    rustls::CertificateError::UnknownIssuer => help::cert_unknown_issuer(ctx),
+                    rustls::CertificateError::InvalidPurpose => help::cert_invalid_purpose(ctx),
+                    _ => (),
+                }
+            }
         })
         .context("failed to verify certification chain")?;
 
