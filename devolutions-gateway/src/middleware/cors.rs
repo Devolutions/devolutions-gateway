79ffe0ecc1d3b76b3fd67127f4420c36b1c1516b
Benoît CORTIER <bcortier@proton.me>
2024-10-31 01:37:53 +0900

    fix(dgw): allow any header to be set in browser JavaScript HTTP requests
    We don’t expose any header particularly sensitive from the Devolutions
    Gateway, and the future HTTP bridge will always require a token which
    is only issued on a per need basis. In fact, in such cases we actually
    want to allow virtually any header to be used for web-based integration
    of various web services (e.g.: VMware dashboard).
    The restriction imposed by the token requirement is strong enough.

--- devolutions-gateway/src/middleware/cors.rs
+++ devolutions-gateway/src/middleware/cors.rs
@@ -4,7 +4,7 @@ use tower_http::cors::CorsLayer;
 pub fn make_middleware() -> CorsLayer {
     CorsLayer::new()
         .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE, Method::PATCH])
-        .allow_headers([header::AUTHORIZATION, header::CONTENT_TYPE])
+        .allow_headers([tower_http::cors::Any, header::AUTHORIZATION])
         .allow_origin(tower_http::cors::Any)
         .max_age(std::time::Duration::from_secs(7200))
         .allow_credentials(false)
