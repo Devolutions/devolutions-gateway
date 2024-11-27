0d06e1e604012df04daef3b0ea2d7a01b9ba95cb
Vladyslav Nikonov <mail@pacmancoder.xyz>
2024-11-27 23:39:37 +0200

    Fixed invalid agent update code GUID

--- crates/devolutions-agent-shared/src/windows/mod.rs
+++ crates/devolutions-agent-shared/src/windows/mod.rs
@@ -7,4 +7,4 @@ pub use uuid::UuidError;
 /// MSI upgrade code for the Devolutions Gateway.
 pub const GATEWAY_UPDATE_CODE: &str = "{db3903d6-c451-4393-bd80-eb9f45b90214}";
 /// MSI upgrade code for the Devolutions Agent.
-pub const AGENT_UPDATE_CODE: &str = "{c3d81328-f118-4d5d-9a82-b7c31b076755}";
+pub const AGENT_UPDATE_CODE: &str = "{82318d3c-811f-4d5d-9a82-b7c31b076755}";
