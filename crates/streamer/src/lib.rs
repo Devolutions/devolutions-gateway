ec43f09fe9847adc35017d4775f4710207288e57
irving ou <jou@devolutions.net>
2024-12-16 10:30:26 -0500

    review fix

--- crates/streamer/src/lib.rs
+++ crates/streamer/src/lib.rs
@@ -7,5 +7,3 @@ pub use config::StreamingConfig;
 pub use streamer::reopenable_file::ReOpenableFile;
 pub use streamer::signal_writer::SignalWriter;
 pub use streamer::{webm_stream, Signal};
-// We need ebml_iterable for TagIterator::emit_master_end_when_eof method
-use ebml_iterable as _;
