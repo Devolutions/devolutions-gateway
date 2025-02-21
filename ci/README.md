83293cbec62af4ee6aab17cc00344f924d618c14
Allan Zhang <6740989+allan2@users.noreply.github.com>
2025-02-21 13:42:19 -0500

    build: split tlk.ps1 into separate scripts (#1240)
    This commit splits _tlk.ps1_. It strives to make certain parts easier to
    run and to make the building and packaging process more readable.
    
    The documentation of how to use the new scripts is in
    [_ci/README.md_](https://github.com/Devolutions/devolutions-gateway/blob/e98ef1a3368cffc352d9cae0183051adabd31945/ci/README.md).
    The existing _tlk.ps1_ is untouched as this PR does not include
    integration with GitHub Actions.
    
    - usage is more strict; unsupported configurations (building on
    supported platforms, unused arguments) will throw an error
    - _build.ps1_ now uses the `target/profile` build by default, without
    having to build into a separate target/arch folder
    - architecture is an optional parameter for _build.ps1_, detecting your
    system architecture if not specified
    - output directory structure now uses the full target triplet
    - this is to enable building for additional targets in the future such
    as musl
    - removed `cargo test` functionality
    - removed unused verbosed flag in `TlkRecipe`
    - removed unused `Get-DotEnvFile` function
    - removed DGATEWAY_PSMODULE_CLEAN functionality
    - fail with `Stop` instead of `SilentlyContinue` when copying files
    - use of named parameters for the PS scripts instead of environment
    variables
    - this makes it clear up-front what is required when building or
    packaging a given product
    - in the future, I would like to pass more parameters to `MSBuild` as
    properties instead of through environment variables
    - functions are named using PowerShell-approved verbs
    - adds documentation
    
    This commit contains all scripts for building on all platforms and
    packaging on Windows.

---
+++ ci/README.md
@@ -0,0 +1,36 @@
+# CI
+
+This folder contains PowerShell scripts for CI, building, and packaging.
+
+## Product Building Guide
+
+- _build.ps1_ - wrapper around `cargo build` that saves to an output folder
+  - handles all products and modules
+- _copy-psmodule.ps1_ - copies the PowerShell module to a directory inside of `$Env:Temp`
+- _package-gateway-windows.ps1_ - one-shot packaging the Gateway for Windows
+
+| Product            | Platform            | Procedure                                              |
+|--------------------|---------------------|--------------------------------------------------------|
+| Gateway            | Windows (regular)   | `build.ps1 gateway`<br />`copy-ps-module.ps1`<br />`package-gateway-windows.ps1` |
+| Gateway            | Windows (assembled) | `build.ps1 gateway`<br />`copy-ps-module.ps1`<br />`package-gateway-windows.ps1 -Generate`<br />`package-assembled.ps1 gateway` |
+| Gateway            | Linux               | `build.ps1 gateway`<br />`package-gateway-linux.ps1` (not available yet)  |
+| Agent       | Windows (regular)          | `build.ps1 agent`  <br />`build.ps1 pedm`<br />`build.ps1 session`<br />`package-agent-windows.ps1`          |
+| Agent       | Windows (assembled)        | `build.ps1 agent`  <br />`build.ps1 pedm`<br />`build.ps1 session`<br />`package-agent-windows.ps1 -Generate`<br />`package-assembled.ps1 agent` |
+| Jetsocat    | Windows/macOS/Linux        | `build.ps1 jetsocat`<br />Jetsocat is not packaged.           |
+| Session     | Windows/macOS/Linux        | `build.ps1 session` <br />Session is not packaged.            |
+| PEDM module        | Windows             | `build.ps1 pedm`                                              |
+| PowerShell module  | Windows             | `copy-ps-module.ps1`                                          |
+
+## What is the difference between _Windows (regular)_ and _Windows (assembled)_?
+
+_Windows (regular)_ is the "normal" build process where the MSI is built by WiX but not signed. This is used in _ci.yaml_. _Windows (assembled)_ is a two-step process where the `-Generate` flag is used to build supporting files for the MSI, including DLLs, language transforms, and _cmd_ scripts. The MSI is assembled in second step using _package-assembled.ps1_. The two-step approach is described [here](https://github.com/oleg-shilo/wixsharp/wiki/Developer's-Guide#compiling-wix-project).
+
+## Dependencies
+
+To build on Windows:
+- WiX Toolset v3
+- MSBuild
+
+To build on Linux:
+- fpm (Ruby package)
+- debhelper
---
+++ ci/package-agent-windows.ps1
@@ -0,0 +1,106 @@
+param(
+    [switch] $Generate,
+    [parameter(Mandatory = $true)]
+    [string] $Exe,
+    [parameter(Mandatory = $true)]
+    [string] $PedmDll,
+    [parameter(Mandatory = $true)]
+    [string] $PedmMsix,
+    [parameter(Mandatory = $true)]
+    [string] $SessionExe,
+    [string] $DesktopAgentDir,
+    [string] $OutputDir
+)
+
+# Use TLS 1.2
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+
+Import-Module (Join-Path $PSScriptRoot 'Build')
+
+# Builds an MSI for Agent.
+#
+# Usage
+#
+# Regular build:
+# New-AgentMsi -Exe $Exe -PedmDll $PedmDll -PedmMsix $PedmMsix -SessionExe $SessionExe -DesktopAgentDir $DesktopAgentDir -OutputDir $OutputDir
+#
+# Generate:
+# New-AgentMsi -Generate -Exe $Exe -PedmDll $PedmDll -PedmMsix $PedmMsix -SessionExe $SessionExe -DesktopAgentDir $DesktopAgentDir
+function New-AgentMsi() {
+    param(
+        # Generates additional files for the MSI. The MSI is not copied to the output directory if this is set. This produces files `package\WindowsManaged\Release\en-US` and `package\WindowsManaged\Release\fr-FR`.
+        [switch] $Generate,
+        [parameter(Mandatory = $true)]
+        # The path to the devolutions-agent.exe file.
+        [string] $Exe,
+        [parameter(Mandatory = $true)]
+        # The path to the devolutions_pedm_shell_ext.dll file.
+        [string] $PedmDll,
+        [parameter(Mandatory = $true)]
+        # The path to the devolutions-agent-pedm.msix.
+        [string] $PedmMsix,
+        [parameter(Mandatory = $true)]
+        # The path to the devolutions-session.exe file.
+        [string] $SessionExe,
+        # The path to the dotnet/DesktopAgent directory.
+        [string] $DesktopAgentDir,
+        # Only required if `Generate` is not set.
+        [string] $OutputDir
+    )
+
+    if ($Generate) {
+        if ($OutputDir) {
+            throw 'Output directory must not be specified when called with -Generate'
+        }
+    }
+    else {
+        if (-not $OutputDir) {
+            throw 'Output directory must be specified when called without -Generate'
+        }
+        elseif (-not (Test-Path $OutputDir)) {
+            throw "Output directory does not exist: $OutputDir"
+        }
+    }
+
+    Set-EnvVarPath 'DAGENT_EXECUTABLE' $Exe
+    Set-EnvVarPath 'DAGENT_PEDM_SHELL_EXT_DLL' $PedmDll
+    Set-EnvVarPath 'DAGENT_PEDM_SHELL_EXT_MSIX' $PedmMsix
+    Set-EnvVarPath 'DAGENT_SESSION_EXECUTABLE' $SessionExe
+    Set-EnvVarPath 'DAGENT_DESKTOP_AGENT_PATH' $DesktopAgentDir
+  
+    $version = Get-Version
+
+    Push-Location
+
+    $repoDir = Split-Path -Parent $PSScriptRoot # currently in `ci`
+    Set-Location (Join-Path $repoDir 'package\AgentWindowsManaged')
+
+    # Set the MSI version, which is read by `package/WindowsManaged/Program.cs`.
+    $Env:DAGENT_VERSION = $version.Substring(2)
+    if ($Generate) {
+        # This is used by `package/WindowsManaged/Program.cs`.
+        $Env:DAGENT_MSI_SOURCE_ONLY_BUILD = '1'
+       
+        foreach ($lang in Get-PackageLanguages) {
+            $Env:DAGENT_MSI_LANG_ID = $lang.Name
+            & 'MSBuild.exe' 'DevolutionsAgent.sln' '/t:restore,build' '/p:Configuration=Release' | Out-Host
+            if ($LASTEXITCODE -ne 0) {
+                throw "Failed to build MSI for language $lang"
+            }
+        }
+    }
+    else {
+        & 'MSBuild.exe' 'DevolutionsAgent.sln' '/t:restore,build' '/p:Configuration=Release' | Out-Host
+        if ($LASTEXITCODE -ne 0) {
+            throw "Failed to build MSI"
+        }
+
+        # When called without `Generate` switch, such as in the regular CI flow, copy the MSI to the output directory.
+        $msi = Join-Path 'Release' 'DevolutionsAgent.msi'
+        Copy-Item -Path $msi -Destination $OutputDir -ErrorAction Stop
+        Write-Output "Copied MSI to $(Join-Path $OutputDir $msi)"
+    }
+    Pop-Location
+}
+
+New-AgentMsi -Generate:($Generate.IsPresent) -Exe $Exe -PedmDll $PedmDll -PedmMsix $PedmMsix -SessionExe $SessionExe -DesktopAgentDir $DesktopAgentDir -OutputDir $OutputDir
