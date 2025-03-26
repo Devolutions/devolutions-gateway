01b14e691a4b43f1fe28559a409a58e1b2b1c1f5
Allan Zhang <6740989+allan2@users.noreply.github.com>
2025-03-26 15:46:22 -0400

    Format using stable fmt

--- crates/devolutions-pedm/src/api/elevate_session.rs
+++ crates/devolutions-pedm/src/api/elevate_session.rs
@@ -6,9 +6,7 @@ use crate::{elevations, policy};
 
 use super::NamedPipeConnectInfo;
 
-pub async fn post_elevate_session(
-    Extension(named_pipe_info): Extension<NamedPipeConnectInfo>,
-) -> Result<(), Error> {
+pub async fn post_elevate_session(Extension(named_pipe_info): Extension<NamedPipeConnectInfo>) -> Result<(), Error> {
     let policy = policy::policy().read();
 
     if let Some(profile) = policy.user_current_profile(&named_pipe_info.user) {
