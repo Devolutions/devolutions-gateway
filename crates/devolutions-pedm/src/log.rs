ebba6a71766c269b66650467cf02e4e942310ec7
Richard Markiewicz <rmarkiewicz@devolutions.net>
2025-05-06 14:05:48 -0400

    fix(pedm): don't panic on missing log_elevation function

--- crates/devolutions-pedm/src/log.rs
+++ crates/devolutions-pedm/src/log.rs
@@ -2,5 +2,5 @@ use devolutions_pedm_shared::policy::ElevationResult;
 
 // The previous function was commented out. The function call for `log_elevation` in `validate_elevation` still remains.
 pub(crate) fn log_elevation(_res: &ElevationResult) -> anyhow::Result<()> {
-    unimplemented!();
+    Ok(())
 }
