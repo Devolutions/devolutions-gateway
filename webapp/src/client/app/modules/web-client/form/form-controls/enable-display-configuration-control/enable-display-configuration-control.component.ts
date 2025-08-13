37f96492480edbe5c190aefef1ed5588ea324172
Krista House <khouse@devolutions.net>
2025-08-13 11:01:46 -0400

    chore(webapp): primeng migration update - ReactiveFormsModule

--- webapp/src/client/app/modules/web-client/form/form-controls/enable-display-configuration-control/enable-display-configuration-control.component.ts
+++ webapp/src/client/app/modules/web-client/form/form-controls/enable-display-configuration-control/enable-display-configuration-control.component.ts
@@ -3,17 +3,18 @@ import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 import { BaseComponent } from '@shared/bases/base.component';
 import { WebFormService } from '@shared/services/web-form.service';
+import { Checkbox } from "primeng/checkbox";
 
 @Component({
   selector: 'web-client-enable-display-configuration-control',
   templateUrl: 'enable-display-configuration-control.component.html',
   styleUrls: ['enable-display-configuration-control.component.scss'],
   standalone: true,
-  imports: [ReactiveFormsModule]
+  imports: [ReactiveFormsModule, Checkbox]
 })
 export class EnableDisplayConfigurationControlComponent extends BaseComponent implements OnInit {
   @Input() parentForm: FormGroup;
-  @Input() inputFormData;
+  @Input() inputFormData: unknown;
 
   constructor(private formService: WebFormService) {
     super();
