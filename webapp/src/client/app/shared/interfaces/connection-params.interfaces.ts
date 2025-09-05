a42336dbb1941bd74a3315c2ab1b3128671b4dd0
Alex Yusiuk <aleksandr.yusuk@apriorit.com>
2025-09-05 14:29:07 +0300

    feat(webapp): add color format selector for VNC session
    This way a user can tune the session for their environment (e.g, lower image quality if the network is slow).
    
    Closes https://github.com/Devolutions/IronVNC/issues/908.

--- webapp/src/client/app/shared/interfaces/connection-params.interfaces.ts
+++ webapp/src/client/app/shared/interfaces/connection-params.interfaces.ts
@@ -36,6 +36,7 @@ export interface IronVNCConnectionParameters {
   token?: string;
   screenSize?: DesktopSize;
   enabledEncodings?: string;
+  colorFormat: string;
   enableCursor: boolean;
   enableExtendedClipboard: boolean;
   ultraVirtualDisplay: boolean;
