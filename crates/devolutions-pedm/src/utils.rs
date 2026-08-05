fe964cc4e59495ec8a73c31b6dab6721c667450e
Benoît CORTIER <git.divisible626@passmail.com>
2026-08-05 21:47:48 +0900

    .

--- crates/devolutions-pedm/src/utils.rs
+++ crates/devolutions-pedm/src/utils.rs
@@ -4,13 +4,13 @@ use std::path::Path;
 
 use devolutions_pedm_shared::policy::{Hash, User};
 use digest::Update;
-use sha1::{Digest as _, Sha1};
-use sha2::{Digest as _, Sha256};
+use sha1::Sha1;
+use sha2::Sha256;
 use tracing::info;
 use win_api_wrappers::fs::create_directory;
 use win_api_wrappers::identity::account::Account;
 use win_api_wrappers::identity::sid::Sid;
-use win_api_wrappers::process::{Process, Process, create_process_as_user, create_proces};
+use win_api_wrappers::process::{ProcessInformation, StartupInfo, create_process_as_user};
 use win_api_wrappers::raw::Win32::System::Threading::PROCESS_CREATION_FLAGS;
 use win_api_wrappers::token::Token;
 use win_api_wrappers::utils::CommandLine;
@@ -68,8 +68,8 @@ impl MultiHasher {
     }
 
     pub(crate) fn finalize(self) -> Hash {
-        let sha1 = self.sha1.finalize();
-        let sha256 = self.sha256.finalize();
+        let sha1 = sha1::Digest::finalize(self.sha1);
+        let sha256 = sha2::Digest::finalize(self.sha256);
 
         Hash {
             sha1: base16ct::lower::encode_string(&sha1),
@@ -80,8 +80,8 @@ impl MultiHasher {
 
 impl Update for MultiHasher {
     fn update(&mut self, data: &[u8]) {
-        self.sha1.update(data);
-        self.sha256.update(data);
+        sha1::Digest::update(&mut self.sha1, data);
+        sha2::Digest::update(&mut self.sha256, data);
     }
 }
 
